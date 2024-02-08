bf584f685a2fe51a5cda783f04ff82fca29c1278
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-02-08 22:40:09 +0530

    Time: 2 ms (46.57%) | Memory: 7.1 MB (12.49%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -11,7 +11,21 @@ public:
         return dp[ind] = f+s;
     }
     int climbStairs(int n) {
-        vector<int>dp(n,-1);
-        return fun(n-1,dp);
+        // vector<int>dp(n,0);
+
+        if(n==1) return 1;
+
+        int prev=1,cur=2;
+        // dp[0] = 1;
+        // dp[1] = 2;
+        for(int ind=2;ind<n;ind++){
+            int f = 0,s=0;
+            if(ind>=1) f = cur;
+            if(ind>=2) s = prev;
+            
+            prev = cur;
+            cur = f+s;
+        }
+        return cur;
     }
 };
