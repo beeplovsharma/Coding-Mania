8281dffd602a858543c70ee0c4927d15f2ae8262
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-04-20 13:55:55 +0530

    Time: 3 ms (96.77%) | Memory: 8.2 MB (61.33%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
 
+        // OPTIMAL
         int n = s.size();
         int l=0,r = 0;
         vector<int>hash(26,0);
@@ -11,13 +12,9 @@ public:
 
             maxFreq = max(maxFreq,hash[s[r]-'A']);
 
-            while((r-l+1)-maxFreq > k){
+            if((r-l+1)-maxFreq > k){
                 hash[s[l]-'A']--;
                 l++;
-
-                maxFreq = 0;
-
-                for(int i=0;i<26;i++) maxFreq = max(maxFreq,hash[i]);
             }
 
             if((r-l+1)-maxFreq <= k) maxLen = max(maxLen,r-l+1);
@@ -25,6 +22,31 @@ public:
             r++;
         }
         return maxLen;
+
+        // BETTER
+        // int n = s.size();
+        // int l=0,r = 0;
+        // vector<int>hash(26,0);
+        // int maxFreq = 0, maxLen = 0;
+        // while(r<n){
+        //     hash[s[r]-'A']++;
+
+        //     maxFreq = max(maxFreq,hash[s[r]-'A']);
+
+        //     while((r-l+1)-maxFreq > k){
+        //         hash[s[l]-'A']--;
+        //         l++;
+
+        //         maxFreq = 0;
+
+        //         for(int i=0;i<26;i++) maxFreq = max(maxFreq,hash[i]);
+        //     }
+
+        //     if((r-l+1)-maxFreq <= k) maxLen = max(maxLen,r-l+1);
+
+        //     r++;
+        // }
+        // return maxLen;
 
         // int n = s.size();
         // int maxLen = 0;
