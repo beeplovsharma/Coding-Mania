9f651cdb2255e1f57b8bd7377c0a8960ae528aad
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-04-20 13:47:27 +0530

    Time: 8 ms (63.88%) | Memory: 8.2 MB (61.33%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,23 +1,48 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+
         int n = s.size();
-        int maxLen = 0;
-        for(int i=0;i<n;i++){
-            vector<int>hash(26,0);
-            int maxFreq = 0;
-            for(int j=i;j<n;j++){
-                hash[s[j]-'A']++;
+        int l=0,r = 0;
+        vector<int>hash(26,0);
+        int maxFreq = 0, maxLen = 0;
+        while(r<n){
+            hash[s[r]-'A']++;
 
-                maxFreq = max(maxFreq,hash[s[j]-'A']);
-                int changes = (j-i+1) - maxFreq;
+            maxFreq = max(maxFreq,hash[s[r]-'A']);
 
-                if(changes <= k){
-                    maxLen = max(maxLen,j-i+1);
-                } 
-                else break;
+            while((r-l+1)-maxFreq > k){
+                hash[s[l]-'A']--;
+                l++;
+
+                maxFreq = 0;
+
+                for(int i=0;i<26;i++) maxFreq = max(maxFreq,hash[i]);
             }
+
+            if((r-l+1)-maxFreq <= k) maxLen = max(maxLen,r-l+1);
+
+            r++;
         }
         return maxLen;
+
+        // int n = s.size();
+        // int maxLen = 0;
+        // for(int i=0;i<n;i++){
+        //     vector<int>hash(26,0);
+        //     int maxFreq = 0;
+        //     for(int j=i;j<n;j++){
+        //         hash[s[j]-'A']++;
+
+        //         maxFreq = max(maxFreq,hash[s[j]-'A']);
+        //         int changes = (j-i+1) - maxFreq;
+
+        //         if(changes <= k){
+        //             maxLen = max(maxLen,j-i+1);
+        //         } 
+        //         else break;
+        //     }
+        // }
+        // return maxLen;
     }
 };
