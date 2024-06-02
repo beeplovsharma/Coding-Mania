a5a9ab8b1ed068426e10b9b45218d9660bf4ca76
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-06-02 14:55:14 +0530

    Time: 27 ms (33.52%) | Memory: 22.7 MB (68.45%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,25 +1,47 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& arr) {
+
         vector<vector<int>> ans;
         int n = arr.size();
         sort(arr.begin(),arr.end());
         for(int i=0;i<n;i++){
             int start = arr[i][0];
             int end = arr[i][1];
-
-            if(!ans.empty() && start <= ans.back()[1]) continue;
-
-            for(int j = i+1;j<n;j++){
-                if(arr[j][0]<=end){
-                    end = max(end,arr[j][1]);
-                }
-                else{
-                    break;
-                }
+            if(ans.empty()){
+                ans.push_back({start,end});
+                continue;
             }
-            ans.push_back({start,end});
+            if(start<=ans.back()[1]){
+                ans.back()[1] = max(end,ans.back()[1]);
+
+            }
+            else{
+                ans.push_back({start,end});
+            }
         }
         return ans;
+
+
+        // vector<vector<int>> ans;
+        // int n = arr.size();
+        // sort(arr.begin(),arr.end());
+        // for(int i=0;i<n;i++){
+        //     int start = arr[i][0];
+        //     int end = arr[i][1];
+
+        //     if(!ans.empty() && start <= ans.back()[1]) continue;
+
+        //     for(int j = i+1;j<n;j++){
+        //         if(arr[j][0]<=end){
+        //             end = max(end,arr[j][1]);
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+        //     ans.push_back({start,end});
+        // }
+        // return ans;
     }
 };
