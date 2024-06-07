7c9c3a9e40aec261b6d9fcc23b225a3c0281fa48
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-06-07 12:34:41 +0530

    Time: 68 ms (6.53%) | Memory: 17.6 MB (6.65%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,19 +1,43 @@
 class Solution {
 public:
-    int numberOfSubstrings(string s) {
-
+    int solve(string s,int k){
+        int l = 0 , r = 0;
         int n = s.size();
         int cnt = 0;
-        vector<int>hash(256,-1);
-        int r = 0;
+        unordered_map<char,int>mp;
         while(r<n){
-            hash[s[r]] = r;
-            int mini = min(hash['a'],min(hash['b'],hash['c']));
-            cnt = cnt + (1+mini);
+            mp[s[r]]++;
+
+            while(l<=r && mp.size()>k){
+                mp[s[l]]--;
+                if(mp[s[l]]==0) mp.erase(s[l]);
+                l++;
+            }
+
+            if(mp.size()<=k) cnt += r-l+1;
             r++;
         }
         return cnt;
+    }
+    int numberOfSubstrings(string s) {
 
+        int x = solve(s,3);
+        int y = solve(s,2);
+        return (x-y);
+
+        // int n = s.size();
+        // int cnt = 0;
+        // vector<int>hash(3,-1);
+        // int r = 0;
+        // while(r<n){
+        //     hash[s[r]-'a'] = r;
+        //     int mini = min(hash[0],min(hash[1],hash[2]));
+        //     cnt = cnt + (1+mini);
+        //     r++;
+        // }
+        // return cnt;
+
+        // Brute
         // int n = s.size();
         // int cnt = 0;
         // for(int i=0;i<n;i++){
