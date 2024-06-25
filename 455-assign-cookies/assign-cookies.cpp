1bb6d377600847ec1de52de7a674fb05f5c98104
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-06-25 14:58:05 +0530

    Time: 27 ms (16.09%) | Memory: 20.5 MB (64.44%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        map<int,int>mp;
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
 
-        for(auto x:s){
-            mp[x]++;
-        }
-        int cnt=0;
-        for(int i=0;i<g.size();i++){
-            auto it = mp.lower_bound(g[i]);
-            if(it != mp.end()){
-                cnt++;
-                mp[it->first]--;
-                if(mp[it->first]==0) mp.erase(it->first);
+        int l=0,r=0;
+        int n = g.size(),m = s.size();
+        while(l<n && r<m){
+            if(s[r]>=g[l]){
+                l++;
+                r++;
+            }
+            else{
+                r++;
             }
         }
-        return cnt;
+        return l;
     }
+    // int findContentChildren(vector<int>& g, vector<int>& s) {
+    //     map<int,int>mp;
+
+    //     for(auto x:s){
+    //         mp[x]++;
+    //     }
+    //     int cnt=0;
+    //     for(int i=0;i<g.size();i++){
+    //         auto it = mp.lower_bound(g[i]);
+    //         if(it != mp.end()){
+    //             cnt++;
+    //             mp[it->first]--;
+    //             if(mp[it->first]==0) mp.erase(it->first);
+    //         }
+    //     }
+    //     return cnt;
+    // }
 };
