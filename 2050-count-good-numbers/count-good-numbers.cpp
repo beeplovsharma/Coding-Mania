4ca9d31f9e9fb22ebb944587cb21ec534dedac02
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-06-28 12:30:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (48.02%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -6,11 +6,15 @@ class Solution {
 public:
 long long power(long long x, long long y) {
     long long res = 1;
-    while (y > 0) {
-        if (y & 1)
+    while (y != 0) {
+        if (y & 1){
             res = (res * x) % mod;
-        y = y >> 1;
-        x = (x * x) % mod;
+            y--;
+        }
+        else{
+            y = y >> 1;
+            x = (x * x) % mod;
+        }
     }
     return res;
 }
