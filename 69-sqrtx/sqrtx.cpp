ed0b565103a44a0bb59434fbf8903f1146bceeb6
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-06-29 18:27:37 +0530

    Time: 51 ms (5.59%) | Memory: 7.4 MB (80.64%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,16 +1,13 @@
 class Solution {
+#define ll long long
 public:
     int mySqrt(int x) {
-        if (x < 0) return -1; // Handle negative input
-    if (x == 0) return 0; // Handle zero input
-    int low = 1;
-    int high = x;
-    while (low <= high) {
-        int mid = low + (high - low) / 2; // Avoid potential overflow
-        if (mid == x / mid) return mid; // Avoid potential overflow
-        else if (mid < x / mid) low = mid + 1;
-        else high = mid - 1;
-    }
-    return high;
+        int ans = 0;
+        for(ll i=1;i*i<=x;i++){
+            if(i <= x/i){
+                ans = i;
+            }
+        }
+        return ans;
     }
 };
