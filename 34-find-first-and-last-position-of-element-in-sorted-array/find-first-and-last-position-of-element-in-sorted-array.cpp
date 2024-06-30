261f005497c923b0e818f11500d471d54a96069a
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-06-30 16:26:02 +0530

    Time: 5 ms (63.07%) | Memory: 15.9 MB (86.47%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,50 +1,44 @@
 class Solution {
 public:
 
-int lbound(vector<int>&v,int x){
+int first(vector<int>&v,int x){
     int low = 0;
     int high = v.size()-1;
-    int ans = 0;
+    int possibleInd = -1;
     while(low<=high){
         int mid= (low+high)/2;
 
-        if(x<=v[mid]){
-            ans = mid;
+        if(v[mid]==x){
+            possibleInd = mid;
             high = mid-1;
         }
-        else{
+
+        else if(x>v[mid]) low = mid+1;
+        else high = mid-1;
+    }
+    return possibleInd;
+}
+int last(vector < int > & v, int x) {
+    int low = 0;
+    int high = v.size()-1;
+    int possibleInd = -1;
+    while(low<=high){
+        int mid= (low+high)/2;
+
+        if(v[mid]==x){
+            possibleInd = mid;
             low = mid+1;
         }
-    }
-    return ans;
-}
-int ubound(vector < int > & v, int key) {
-  int start = 0;
-  int end = v.size() - 1;
-  int res = -1;
 
-  while (start <= end) {
-    int mid = start + (end - start) / 2;
-    if (v[mid] == key) {
-      res = mid;
-      start = mid + 1;
-
-    } else if (key < v[mid]) {
-      end = mid - 1;
-    } else {
-      start = mid + 1;
+        else if(x>v[mid]) low = mid+1;
+        else high = mid-1;
     }
-  }
-  return res;
+    return possibleInd;
 }
 
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
-        int lb = lbound(nums,target);
-        int ub = ubound(nums,target);
-
-        if(lb==n || nums[lb]!=target) return {-1,-1};
-
-        return {lb,ub};
+        int firstInd = first(nums,target);
+        int lastInd = last(nums,target);
+        return {firstInd,lastInd};
     }
 };
