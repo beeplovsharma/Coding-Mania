f58e0997979fcd46ab71519b06c9bb8c46323267
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-07-03 19:33:05 +0530

    Time: 4 ms (70.51%) | Memory: 12.3 MB (12.32%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -2,45 +2,41 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
 
-        int n1=nums1.size(),n2=nums2.size();
-        unordered_map<int,int>mp;
-        for(int i=0;i<n2;i++){
-            int found = 0;
-            for(int j=i+1;j<n2;j++){
-                if(nums2[j]>nums2[i]){
-                    mp[nums2[i]]=nums2[j];
-                    found = 1;
-                    break;
-                }
-            }
-            if(found==0) mp[nums2[i]]=-1;
-        }
-        vector<int>ans;
-        for(auto x:nums1){
-            ans.push_back(mp[x]);
-        }
-        return ans;
-
         // int n1=nums1.size(),n2=nums2.size();
-        // stack<int>st;
         // unordered_map<int,int>mp;
-        // for(int i=n2-1;i>=0;i--){
-        //     int x = nums2[i];
-        //     if(st.empty()) mp[x]=-1;
-        //     if(st.top()>x && !st.empty()) mp[x] = st.top();
-        //     else{
-        //         while(st.top()<=x && !st.empty()){
-        //             st.pop();
+        // for(int i=0;i<n2;i++){
+        //     int found = 0;
+        //     for(int j=i+1;j<n2;j++){
+        //         if(nums2[j]>nums2[i]){
+        //             mp[nums2[i]]=nums2[j];
+        //             found = 1;
+        //             break;
         //         }
-        //         if(!st.empty()) mp[x] = st.top();
-        //         else mp[x] = -1;
         //     }
-        //     st.push(x);
+        //     if(found==0) mp[nums2[i]]=-1;
         // }
         // vector<int>ans;
         // for(auto x:nums1){
         //     ans.push_back(mp[x]);
         // }
         // return ans;
+
+        unordered_map<int,int>mp;
+        stack<int>st;
+        int n = nums2.size();
+        for(int i=n-1;i>=0;i--){
+            int ele = nums2[i];
+            while(!st.empty() && st.top()<=ele){
+                st.pop();
+            }
+            int res = (st.empty()) ? -1 : st.top();
+            mp[ele] = res;
+            st.push(ele);
+        }
+        vector<int>ans;
+        for(auto x:nums1){
+            ans.push_back(mp[x]);
+        }
+        return ans;
     }
 };
