a3280a2121ab1197f5886b9a35ed3c3d3aa8c6a2
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-08-15 18:50:26 +0530

    Time: 1174 ms (29.02%) | Memory: 360.5 MB (23.54%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int source,
-                   int destination) {
-        unordered_map<int, vector<int>> adj;
-        for (auto edge : edges) {
+    void BFS(unordered_map<int,vector<int>>&adj,vector<bool>&visited){
+        queue<int>q;
+        q.push(0);
+        visited[0] = true;
+
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            
+            for(auto x:adj[u]){
+                if(!visited[x]){
+                    visited[x] = true;
+                    q.push(x);
+                }
+            }
+        }
+    }
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        unordered_map<int,vector<int>>adj;
+
+        for(auto edge:edges){
             int u = edge[0];
             int v = edge[1];
 
@@ -11,25 +28,25 @@ public:
             adj[v].push_back(u);
         }
 
-        vector<bool> vis(n + 1, false);
-        queue<int> q;
+        vector<bool>visited(n+1,false);
+        queue<int>q;
         q.push(source);
-        vis[source] = true;
+        visited[source] = true;
 
-        while (!q.empty()) {
-            int node = q.front();
+        while(!q.empty()){
+            int u = q.front();
             q.pop();
-
-            if(vis[destination]==true) return true;
-
-            for (auto x : adj[node]) {
-
-                if (!vis[x]) {
-                    vis[x] = true;
+            
+            for(auto x:adj[u]){
+                if(!visited[x]){
+                    visited[x] = true;
                     q.push(x);
                 }
             }
         }
-        return false;
+        
+        if(visited[destination]==false) return false;
+
+        return true;
     }
 };
