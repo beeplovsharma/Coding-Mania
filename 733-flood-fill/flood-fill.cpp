4146d2644ecd9c18df282f8b97229beb3f39c165
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-08-15 23:04:49 +0530

    Time: 8 ms (47.26%) | Memory: 16.9 MB (70.61%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    bool isValid(vector<vector<int>>& image,int target,int i,int j, int n, int m, int color){
+    bool isValid(vector<vector<int>>& image,int target,int i,int j, int n, int m){
         if(i>=0 && i<n && j>=0 && j<m && image[i][j]==target) return true;
         return false;
     }
     void DFS(vector<vector<int>>& image,int target,int i,int j, int n, int m, int color){
         image[i][j] = color;
 
-        if(isValid(image,target,i+1,j,n,m,color)) DFS(image,target,i+1,j,n,m,color);
-        if(isValid(image,target,i-1,j,n,m,color)) DFS(image,target,i-1,j,n,m,color);
-        if(isValid(image,target,i,j-1,n,m,color)) DFS(image,target,i,j-1,n,m,color);
-        if(isValid(image,target,i,j+1,n,m,color)) DFS(image,target,i,j+1,n,m,color);
+        if(isValid(image,target,i+1,j,n,m)) DFS(image,target,i+1,j,n,m,color);
+        if(isValid(image,target,i-1,j,n,m)) DFS(image,target,i-1,j,n,m,color);
+        if(isValid(image,target,i,j-1,n,m)) DFS(image,target,i,j-1,n,m,color);
+        if(isValid(image,target,i,j+1,n,m)) DFS(image,target,i,j+1,n,m,color);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n = image.size();
