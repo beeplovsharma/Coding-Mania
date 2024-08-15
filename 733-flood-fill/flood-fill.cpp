f8824c3f70e75968820a3ad6bf60563be8f0616e
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-08-15 23:01:47 +0530

    Time: 0 ms (100.00%) | Memory: 16.9 MB (70.61%) - LeetSync

---
+++ 733-flood-fill/flood-fill.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(vector<vector<int>>& image,int target,int i,int j, int n, int m, int color){
+        if(i>=0 && i<n && j>=0 && j<m && image[i][j]==target) return true;
+        return false;
+    }
+    void DFS(vector<vector<int>>& image,int target,int i,int j, int n, int m, int color){
+        image[i][j] = color;
+
+        if(isValid(image,target,i+1,j,n,m,color)) DFS(image,target,i+1,j,n,m,color);
+        if(isValid(image,target,i-1,j,n,m,color)) DFS(image,target,i-1,j,n,m,color);
+        if(isValid(image,target,i,j-1,n,m,color)) DFS(image,target,i,j-1,n,m,color);
+        if(isValid(image,target,i,j+1,n,m,color)) DFS(image,target,i,j+1,n,m,color);
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n = image.size();
+        int m = image[0].size();
+        int target = image[sr][sc];
+        if(target != color)
+            DFS(image,target,sr,sc,n,m,color);
+        return image;
+    }
+};
