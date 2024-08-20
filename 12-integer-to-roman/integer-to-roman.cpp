29128ce34aed53c0578d6d24256ab210df4f9aff
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-08-20 23:53:57 +0530

    Time: 3 ms (87.32%) | Memory: 13.1 MB (35.96%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,37 +1,19 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        map<int,string>mp;
-        mp[1] = "I";
-        mp[4] = "IV";
-        mp[5] = "V";
-        mp[9] = "IX";
-        mp[10] = "X";
-        mp[40] = "XL";
-        mp[50] = "L";
-        mp[90] = "XC";
-        mp[100] = "C";
-        mp[400] = "CD";
-        mp[500] = "D";
-        mp[900] = "CM";
-        mp[1000] = "M";
-        vector<int>temp = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        vector<int>val = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        vector<string>sym = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         string ans = "";
-        while(num!=0){
-            int div = 0;
-            for(auto x:temp){
-                if(x<=num){
-                    div = x;
-                    break;
-                }
+        for(int i=0;i<13;i++){
+            if(num==0) break;
+
+            int times = num/val[i];
+            
+            while(times--){
+                ans += sym[i];
             }
-            int times = num/div;
-            while(times!=0){
-                ans+=mp[div];
-                times--;
-            }
-            int rem = num % div;
-            num = rem;
+
+            num = num % val[i];
         }
         return ans;
     }
