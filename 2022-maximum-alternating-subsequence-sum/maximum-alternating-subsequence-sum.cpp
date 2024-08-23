a64016e5968d919119c24beb8cd33144e4208012
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-08-23 22:51:16 +0530

    Time: 240 ms (50.16%) | Memory: 155.9 MB (39.38%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -14,7 +14,16 @@ public:
     }
     long long maxAlternatingSum(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<ll>>dp(n+1,vector<ll>(2,-1));
-        return fun(nums,0,1,dp);
+        vector<vector<ll>>dp(n+1,vector<ll>(2,0));
+        for(int ind=n-1;ind>=0;ind--){
+            for(int isEven = 0;isEven<2;isEven++){
+                ll notpick = dp[ind+1][isEven];
+                ll val = isEven ? nums[ind] : -nums[ind];
+                ll pick = val + dp[ind+1][!isEven];
+
+                dp[ind][isEven] = max(pick,notpick);
+            }
+        }
+        return dp[0][1];
     }
 };
