5debaac58965c5b1ff2a634ea4ce93ddfe9b659a
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-08-23 16:38:44 +0530

    Time: 3 ms (29.59%) | Memory: 7.6 MB (28.45%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
-    int fun(int ind,vector<int>&dp){
-        if(ind==0) return 1;
-        if(ind==1) return 2;
-        if(dp[ind]!=-1) return dp[ind];
-        int f = 0,s=0;
-        if(ind>=1) f = fun(ind-1,dp);
-        if(ind>=2) s = fun(ind-2,dp);
-        
-        return dp[ind] = f+s;
+    // MEMOIZATION
+    int fun(int n,vector<int>&dp){
+        if(n<0) return 0;
+        if(n==0) return 1;
+
+        if(dp[n]!=-1) return dp[n];
+
+        int one = fun(n-1,dp);
+        int two = fun(n-2,dp);
+
+        return dp[n] = one+two;
     }
     int climbStairs(int n) {
-        // vector<int>dp(n,0);
-
-        if(n==1) return 1;
-
-        int prev=1,cur=2;
-        // dp[0] = 1;
-        // dp[1] = 2;
-        for(int ind=2;ind<n;ind++){
-            int f = 0,s=0;
-            if(ind>=1) f = cur;
-            if(ind>=2) s = prev;
-            
-            prev = cur;
-            cur = f+s;
-        }
-        return cur;
+        vector<int>dp(n+1,-1);
+        return fun(n,dp);
     }
 };
