4bf45da79870442e2fe45c537cd8f49c7981e8d6
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-08-23 22:51:30 +0530

    Time: 274 ms (27.07%) | Memory: 155.9 MB (39.38%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -1,17 +1,7 @@
 class Solution {
 #define ll long long
 public:
-    ll fun(vector<int>& nums,int ind,int isEven,vector<vector<ll>>&dp){
-        if(ind>=nums.size()) return 0;
-
-        if(dp[ind][isEven]!=-1) return dp[ind][isEven];
-
-        ll notpick = fun(nums,ind+1,isEven,dp);
-        ll val = isEven ? nums[ind] : -nums[ind];
-        ll pick = val + fun(nums,ind+1,!isEven,dp);
-
-        return dp[ind][isEven] = max(pick,notpick);
-    }
+// Tabulation
     long long maxAlternatingSum(vector<int>& nums) {
         int n = nums.size();
         vector<vector<ll>>dp(n+1,vector<ll>(2,0));
