b6dd9f5b905aff691c3f806307234e3ce228e3a7
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-08-23 23:45:00 +0530

    Time: 160 ms (44.10%) | Memory: 13.5 MB (44.75%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& arr) {
         int n = arr.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        vector<int>next(n+1,0),cur(n+1,0);
         for(int ind=n-1;ind>=0;ind--){
             for(int prev_ind=ind-1;prev_ind>=-1;prev_ind--){
-                int notPick = 0 + dp[ind+1][prev_ind+1];
+                int notPick = 0 + next[prev_ind+1]; //must do index shifting of prev_ind
                 int pick = 0;
                 if(prev_ind == -1 || arr[ind]>arr[prev_ind]) 
-                    pick = 1 + dp[ind+1][ind+1]; //must do index shifting of prev_ind
+                    pick = 1 + next[ind+1]; //must do index shifting of prev_ind
                 
-                dp[ind][prev_ind+1] = max(pick,notPick);
+                cur[prev_ind+1] = max(pick,notPick);
             }
+            next = cur;
         }
 
-        return dp[0][-1+1];
+        return next[-1+1];
     }
 };
