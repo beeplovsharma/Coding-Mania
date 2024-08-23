09ad1ffba8401bd62306c3758939e421ea027677
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-08-24 01:16:29 +0530

    Time: 14 ms (95.23%) | Memory: 25.8 MB (55.87%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    int fun(int i,int j,string s, string t,vector<vector<int>>&dp){
-
-        if(i==0 || j==0) return 0;
-
+    int fun(string s1, string s2,int i,int j,vector<vector<int>>&dp){
+        if(i>=s1.size() || j>=s2.size()) return 0;
+        
         if(dp[i][j]!=-1) return dp[i][j];
 
-        if(s[i-1]==t[j-1]) return dp[i][j] = 1 + fun(i-1,j-1,s,t,dp);
+        if(s1[i]==s2[j]) return dp[i][j] = 1+fun(s1,s2,i+1,j+1,dp);
 
-        return dp[i][j] = max(fun(i-1,j,s,t,dp),fun(i,j-1,s,t,dp));
+        return dp[i][j] = max(fun(s1,s2,i+1,j,dp),fun(s1,s2,i,j+1,dp));
     }
-
-    int longestCommonSubsequence(string s, string t) {
-        int n1 = s.size(),n2 = t.size();
-        vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
-        // return fun(n1,n2,text1,text2,dp);
-        vector<int>prev(n2+1,0),cur(n2+1,0);
-        for(int i=0;i<=n1;i++) prev[0] = 0;
-        for(int j=0;j<=n2;j++) dp[0][j] = 0;
-
-        for(int i=1;i<=n1;i++){
-            for(int j=1;j<=n2;j++){
-                if(s[i-1]==t[j-1]) cur[j] = 1 + prev[j-1];
-
-                else cur[j] = max(prev[j],cur[j-1]);
+    int longestCommonSubsequence(string s1, string s2) {
+        int n = s1.size(),m = s2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(s1[i]==s2[j])
+                    dp[i][j] = 1+dp[i+1][j+1];
+                else 
+                    dp[i][j] = max(dp[i+1][j],dp[i][j+1]);
             }
-            prev = cur;
         }
-        return cur[n2];
+        return dp[0][0];
     }
 };
