a7d8f6f19f27df6d381577db618d05dcfdecdab7
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-10-25 14:07:21 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (39.73%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,44 +1,35 @@
 class Solution {
 public:
-
-int first(vector<int>&v,int x){
-    int low = 0;
-    int high = v.size()-1;
-    int possibleInd = -1;
-    while(low<=high){
-        int mid= (low+high)/2;
-
-        if(v[mid]==x){
-            possibleInd = mid;
-            high = mid-1;
-        }
-
-        else if(x>v[mid]) low = mid+1;
-        else high = mid-1;
-    }
-    return possibleInd;
-}
-int last(vector < int > & v, int x) {
-    int low = 0;
-    int high = v.size()-1;
-    int possibleInd = -1;
-    while(low<=high){
-        int mid= (low+high)/2;
-
-        if(v[mid]==x){
-            possibleInd = mid;
-            low = mid+1;
-        }
-
-        else if(x>v[mid]) low = mid+1;
-        else high = mid-1;
-    }
-    return possibleInd;
-}
-
     vector<int> searchRange(vector<int>& nums, int target) {
-        int firstInd = first(nums,target);
-        int lastInd = last(nums,target);
-        return {firstInd,lastInd};
+        int n = nums.size();
+        int low=0,high=n-1;
+
+        int flag=-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid]==target){
+                flag = mid;
+                break;
+            }
+            else if(nums[mid]<target) low = mid+1;
+            else high = mid-1;
+        }
+
+        int l=flag,r=flag;
+        while(l>0){
+            if(nums[l-1]!=target){
+                break;
+            }
+            l--;
+        }
+        while(r<n-1){
+            if(nums[r+1]!=target){
+            break;
+            }
+            r++;
+        }
+
+        return {l,r};
     }
 };
