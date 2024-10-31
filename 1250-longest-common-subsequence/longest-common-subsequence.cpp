3d90fe9e402c1049326af31e01d2fe971cf98513
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-10-31 17:34:54 +0530

    Time: 35 ms (30.57%) | Memory: 26 MB (30.42%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string s1, string s2) {
-        int n = s1.size(),m = s2.size();
-        vector<int>next(m+1,0),cur(m+1,0);
-        for(int i=n-1;i>=0;i--){
-            for(int j=m-1;j>=0;j--){
-                if(s1[i]==s2[j])
-                    cur[j] = 1+next[j+1];
-                else 
-                    cur[j] = max(next[j],cur[j+1]);
+    int fun(string &a,string &b,int i,int j,vector<vector<int>>&dp){
+        if(i==0 || j==0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int take = 0,notTake=0;
+        if(a[i-1]==b[j-1])
+            take = 1+fun(a,b,i-1,j-1,dp);
+        else notTake = max(fun(a,b,i-1,j,dp),fun(a,b,i,j-1,dp));
+        
+        return dp[i][j] = max(take,notTake);
+    }
+    int longestCommonSubsequence(string a, string b) {
+        int n = a.size(),m = b.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                int take = 0,notTake=0;
+                if(a[i-1]==b[j-1])
+                    take = 1+dp[i-1][j-1];
+                else notTake = max(dp[i-1][j],dp[i][j-1]);
+                
+                dp[i][j] = max(take,notTake);
             }
-            next = cur;
         }
-        return cur[0];
+        return dp[n][m];
     }
 };
