aa9a2296ffe97fac9bac03a40a19405a257121ef
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-11-01 14:30:06 +0530

    Time: 95 ms (9.36%) | Memory: 83.7 MB (5.04%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -18,10 +18,30 @@ public:
 
         return dp[ind][amount] = min(take,notTake);
     }
-    int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
+    int coinChange(vector<int>& nums, int amt) {
+        int n = nums.size();
         vector<vector<int>>dp(n,vector<int>(1e4+1,-1));
-        int ans = fun(coins,n-1,amount,dp);
+
+        for(int T=0;T<=1e4;T++){
+            if(T%nums[0]==0) dp[0][T] = T/nums[0];
+            else dp[0][T] = 1e9;
+        }
+        for(int i=0;i<n;i++) dp[i][0] = 0;
+
+        for(int ind=1;ind<n;ind++){
+            for(int amount=1;amount<=1e4;amount++){
+                int notTake = dp[ind-1][amount];
+                int take = 1e9;
+                if(nums[ind]<=amount){
+                    take = 1 + dp[ind][amount-nums[ind]];
+        }
+
+
+            dp[ind][amount] = min(take,notTake);
+            }
+        }
+
+        int ans = dp[n-1][amt];
         if(ans==1e9) return -1;
         else return ans;
     }
