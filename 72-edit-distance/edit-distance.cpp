4cd5d3292e7020f82b448cf8faded2bcb345213b
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-11-01 13:34:41 +0530

    Time: 3 ms (88.93%) | Memory: 12.9 MB (39.79%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int fun(string s, string t,int i,int j,vector<vector<int>>&dp){
-        if(i==0) return j;
-        if(j==0) return i;
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        if(s[i-1]==t[j-1]){
-            return dp[i][j] = 0+fun(s,t,i-1,j-1,dp);
-        }
-        else{
-            int replace = 1+fun(s,t,i-1,j-1,dp);
-            int del = 1+fun(s,t,i-1,j,dp);
-            int ins = 1+fun(s,t,i,j-1,dp);
-            return dp[i][j] = min(replace,min(del,ins));
-        }
-    }
     int minDistance(string s, string t) {
         int n = s.size(), m = t.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        for(int j=0;j<=m;j++) dp[0][j] = j;
+        for(int i=0;i<=n;i++) dp[i][0] = i;
 
-        return fun(s,t,n,m,dp);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    int replace = 1+dp[i-1][j-1];
+                    int del = 1+dp[i-1][j];
+                    int ins = 1+dp[i][j-1];
+                    dp[i][j] = min(replace,min(del,ins));
+        }
+            }
+        }
+
+        return dp[n][m];
     }
 };
