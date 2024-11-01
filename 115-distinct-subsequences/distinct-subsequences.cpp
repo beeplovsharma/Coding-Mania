a148f83bfd2ca2ac14a3290ff9e62242374379bf
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-11-01 11:22:47 +0530

    Time: 2798 ms (10.04%) | Memory: 422 MB (6.02%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int fun(int i,int j,string s,string t,vector<vector<int>>&dp){
-
+    int fun(string s,string t,int i,int j,vector<vector<int>>&dp){
         if(j<0) return 1;
         if(i<0) return 0;
 
         if(dp[i][j]!=-1) return dp[i][j];
 
-        if(s[i]==t[j]){
-            return dp[i][j] = fun(i-1,j-1,s,t,dp)+fun(i-1,j,s,t,dp);
-        }
-        else return dp[i][j] = fun(i-1,j,s,t,dp);
+        int one = 0;
+        int two = 0;
+        if(s[i]==t[j])
+            one = fun(s,t,i-1,j-1,dp) + fun(s,t,i-1,j,dp);
+        else
+            two = fun(s,t,i-1,j,dp);
+        
+        return dp[i][j] = one+two;
     }
     int numDistinct(string s, string t) {
-        int n = s.size();
-        int m = t.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return fun(n-1,m-1,s,t,dp);
+        int n = s.size(),m=t.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return fun(s,t,n-1,m-1,dp);
     }
 };
