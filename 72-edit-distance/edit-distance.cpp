a4fbf8e3fca06c6f7c799e6b78f50b6f5a0f3d44
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-11-01 13:50:21 +0530

    Time: 4 ms (77.88%) | Memory: 10.3 MB (94.68%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -2,24 +2,27 @@ class Solution {
 public:
     int minDistance(string s, string t) {
         int n = s.size(), m = t.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        for(int j=0;j<=m;j++) dp[0][j] = j;
-        for(int i=0;i<=n;i++) dp[i][0] = i;
+        vector<int>prev(m+1,0),cur(m+1,0);
+        // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+
+        for(int j=0;j<=m;j++) prev[j] = j;
 
         for(int i=1;i<=n;i++){
+            cur[0] = i;
             for(int j=1;j<=m;j++){
                 if(s[i-1]==t[j-1]){
-                    dp[i][j] = dp[i-1][j-1];
+                    cur[j] = prev[j-1];
                 }
                 else{
-                    int replace = 1+dp[i-1][j-1];
-                    int del = 1+dp[i-1][j];
-                    int ins = 1+dp[i][j-1];
-                    dp[i][j] = min(replace,min(del,ins));
-        }
+                    int replace = 1+prev[j-1];
+                    int del = 1+prev[j];
+                    int ins = 1+cur[j-1];
+                    cur[j] = min(replace,min(del,ins));
+                }
             }
+            prev = cur;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
