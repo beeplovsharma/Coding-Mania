ceb782134b9cc1f842619dd9308dd6acb22b2bdb
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-11-01 11:49:59 +0530

    Time: 35 ms (42.65%) | Memory: 42.9 MB (30.58%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int fun(string s,string t,int i,int j,vector<vector<int>>&dp){
-        if(j<0) return 1;
-        if(i<0) return 0;
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int one = 0;
-        int two = 0;
-        if(s[i]==t[j])
-            one = fun(s,t,i-1,j-1,dp) + fun(s,t,i-1,j,dp);
-        else
-            two = fun(s,t,i-1,j,dp);
-        
-        return dp[i][j] = one+two;
-    }
+    #define ll long long
     int numDistinct(string s, string t) {
         int n = s.size(),m=t.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return fun(s,t,n-1,m-1,dp);
+        vector<vector<double>>dp(n+1,vector<double>(m+1,0));
+
+        for(int i=0;i<=n;i++) dp[i][0] = 1;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                double one = 0;
+                double two = 0;
+                if(s[i-1]==t[j-1])
+                    one = dp[i-1][j-1] + dp[i-1][j];
+                else
+                    two = dp[i-1][j];
+                
+                dp[i][j] = one+two;
+            }
+        }
+        return (int)dp[n][m];
     }
 };
