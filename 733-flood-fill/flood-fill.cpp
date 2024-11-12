373700f33864c2c43e241bea6e85267cac3da08c
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-11-12 19:31:57 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (24.78%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
-    bool isValid(vector<vector<int>>& image,int target,int i,int j, int n, int m){
-        if(i>=0 && i<n && j>=0 && j<m && image[i][j]==target) return true;
-        return false;
-    }
-    void DFS(vector<vector<int>>& image,int target,int i,int j, int n, int m, int color){
-        image[i][j] = color;
-
-        if(isValid(image,target,i+1,j,n,m)) DFS(image,target,i+1,j,n,m,color);
-        if(isValid(image,target,i-1,j,n,m)) DFS(image,target,i-1,j,n,m,color);
-        if(isValid(image,target,i,j-1,n,m)) DFS(image,target,i,j-1,n,m,color);
-        if(isValid(image,target,i,j+1,n,m)) DFS(image,target,i,j+1,n,m,color);
-    }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+    bool isValid(vector<vector<int>>& image, int row, int col, vector<vector<bool>>& isVisited, int initialColor, int newColor) {
         int n = image.size();
         int m = image[0].size();
-        int target = image[sr][sc];
-        if(target != color)
-            DFS(image,target,sr,sc,n,m,color);
+        
+        if (row >= 0 && row < n && col >= 0 && col < m && 
+            image[row][col] == initialColor && !isVisited[row][col] && 
+            image[row][col] != newColor) {
+            return true;
+        }
+        return false;
+    }
+
+    void DFS(vector<vector<int>>& image, int row, int col, int newColor, vector<vector<bool>>& isVisited, int initialColor) {
+        isVisited[row][col] = true;
+        image[row][col] = newColor;
+        
+        // Define four possible directions for traversal (up, down, left, right)
+        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        
+        // Perform DFS in each direction
+        for (auto dir : directions) {
+            int newRow = row + dir.first;
+            int newCol = col + dir.second;
+
+            // Check if moving in the direction is valid
+            if (isValid(image, newRow, newCol, isVisited, initialColor, newColor)) {
+                DFS(image, newRow, newCol, newColor, isVisited, initialColor);
+            }
+        }
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        int n = image.size();
+        int m = image[0].size();
+        vector<vector<bool>> isVisited(n, vector<bool>(m, false));
+        int initialColor = image[sr][sc];
+  
+            DFS(image, sr, sc, newColor, isVisited, initialColor);
         return image;
     }
 };
