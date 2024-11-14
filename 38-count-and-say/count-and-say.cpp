59231d8b2518afdb3131153b23adc5edba944cba
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2024-11-14 18:02:16 +0530

    Time: 3 ms (85.05%) | Memory: 11.3 MB (16.51%) - LeetSync

---
+++ 38-count-and-say/count-and-say.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string fun(string s){
+        string ans = "";
+        int n = s.size();
+        int i=0;
+        int idx = 0;
+        while(i<n){
+            char cur_ch = s[i];
+            int cnt = 0;
+            while(i<n && s[i]==cur_ch){
+                i++;
+                cnt++;
+            }
+            ans+=s[idx];
+            idx++;
+            ans+=to_string(cnt);
+            idx = i;
+        }
+        return ans;
+    }
+    string rec(int n) {
+        if(n==1) return "1";
+        
+        string s = rec(n-1);
+        return fun(s);
+        
+    }
+    
+    string countAndSay(int n) {
+        string ans = rec(n);
+        reverse(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
