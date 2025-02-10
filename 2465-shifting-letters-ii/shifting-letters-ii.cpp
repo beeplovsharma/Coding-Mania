30d1c59f3623ed85d1bb3d4ec9f93a92661ca156
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-02-10 22:36:33 +0530

    Time: 1 ms (83.95%) | Memory: 97.7 MB (95.78%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/shifting-letters-ii.cpp
@@ -3,23 +3,18 @@ public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
         int n = s.size();
         vector<int> temp(n , 0);
-
-        // Apply shift increments and decrements
         for (auto &shift : shifts) {
             int i = shift[0], j = shift[1], direction = shift[2] == 0 ? -1 : 1;
 
             temp[i] += direction;
             if (j + 1 < n) temp[j + 1] -= direction; 
         }
-
-        // Calculate cumulative shift using prefix sum
-        int cumulativeShift = 0;
-        for (int i = 0; i < n; i++) {
-            cumulativeShift += temp[i];
-            int shift = (cumulativeShift % 26 + 26) % 26; // Normalize shifts
-            s[i] = 'a' + (s[i] - 'a' + shift) % 26; // Apply the shift
+        int cumshift = 0;
+        for(int i=0;i<n;i++){
+            cumshift += temp[i];
+            int shift = (cumshift%26+26)%26;
+            s[i] = 'a' + (s[i]-'a'+shift)%26;
         }
-
         return s;
     }
 };
