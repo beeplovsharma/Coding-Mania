1e103550324e5c7d5eac18b9c16cd416ada47c71
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-02-15 12:01:24 +0530

    Time: 20 ms (76.07%) | Memory: 7.8 MB (95.72%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-    bool check(string str,int n,int i,int sum){
-        if(i==str.size()){
-            return sum==n;
-        }
-        if(sum>n) return false;
+    bool check(int sq,int tar,int sum){
+        if(sq==0) return sum==tar;
 
-        for(int j=i;j<str.size();j++){
-            string sub = str.substr(i,j-i+1);
-            int val = stoi(sub);
-            if(check(str,n,j+1,sum+val)) return true;
-        }
-
-        return false;
+        return check(sq/10,tar,sum+sq%10) ||
+        check(sq/100,tar,sum+sq%100) ||
+        check(sq/1000,tar,sum+sq%1000) ||
+        check(sq/10000,tar,sum+sq%10000);
     }
     int punishmentNumber(int n) {
         int ans = 0;
         for(int i=1;i<=n;i++){
             int sq = i*i;
-            string stri = to_string(sq);
-            if(check(stri,i,0,0)){
+            if(check(sq,i,0)){
                 ans += sq;
             }
         }
