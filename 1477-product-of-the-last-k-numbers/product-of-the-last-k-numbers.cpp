3de265e50dc2daa8268937d416a139ef3be38152
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-02-15 10:29:07 +0530

    Time: 31 ms (37.96%) | Memory: 78 MB (76.59%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
@@ -6,16 +6,25 @@ public:
     }
     
     void add(int num) {
-        nums.push_back(num);
+        if(num==0){
+            nums.clear();
+        }
+        else{
+            if(nums.size()==0) nums.push_back(num);
+            else{
+                int prod = nums.back() * num;
+                nums.push_back(prod);
+            }
+        }
     }
     
     int getProduct(int k) {
+        if(k>nums.size()) return 0;
+
         int n = nums.size();
-        int ans = 1;
-        for(int i=n-1;i>=n-k;i--){
-            ans = ans*nums[i];
-        }
-        return ans;
+        if(k==nums.size()) return nums[n-1];
+
+        return nums[n-1]/nums[n-k-1];
     }
 };
 
