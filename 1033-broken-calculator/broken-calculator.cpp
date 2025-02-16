2713bfbb366d743ed1eb350f19b72a2d346565c9
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-02-16 09:28:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (49.85%) - LeetSync

--- 1033-broken-calculator/broken-calculator.cpp
+++ 1033-broken-calculator/broken-calculator.cpp
@@ -3,7 +3,7 @@ public:
     int brokenCalc(int startValue, int target) {
         if(target<=startValue) return (startValue-target);
 
-        if(target%2==0) return 1+brokenCalc(startValue,target/2);
+        if(!(target&1)) return 1+brokenCalc(startValue,target/2);
 
         return 1+brokenCalc(startValue,target+1);
     }
