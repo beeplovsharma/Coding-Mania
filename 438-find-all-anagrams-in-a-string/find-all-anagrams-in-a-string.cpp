b1cd57b7aaa1950164244dc4b7f33ac0620525d9
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-02-20 22:01:40 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (65.10%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
-    bool checkAna(vector<int>&freq){
-        for(int i=0;i<freq.size();i++){
-            if(freq[i]!=0) return false;
+    bool check(vector<int>&hash){
+        for(int i=0;i<hash.size();i++){
+            if(hash[i]!=0) return false;
         }
         return true;
     }
     vector<int> findAnagrams(string s, string p) {
-        vector<int>indices;
-
-        vector<int>freq(26,0);
-        // Counting Freq
-        for(int i=0;i<p.size();i++){
-            freq[p[i]-'a']++;
-        }
-
-        //Sliding Window
+        vector<int>ans;
+        int n = s.size(),m = p.size();
         int i=0,j=0;
-        int n = s.size();
-        int k = p.size(); //pattern size
-
+        vector<int>hash(26,0);
+        for(int i=0;i<p.size();i++) hash[p[i]-'a']++;
         while(j<n){
-            freq[s[j]-'a']--;
-
-            if(j-i+1 == k){
-                if(checkAna(freq)){
-                    indices.push_back(i);
-                }
-
-                freq[s[i]-'a']++;
-                i++;
+            hash[s[j]-'a']--;
+            if(j-i+1<m){
+                j++;
             }
-
-            j++;
+            else{
+                if(check(hash)) ans.push_back(i);
+                hash[s[i]-'a']++;
+                i++;
+                j++;
+            }
         }
+        return ans;
 
-        return indices;
     }
 };
