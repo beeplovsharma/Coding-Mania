b4cdb7f67da2dfc5d2f4b0d123bceac69a1e48bc
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-02-23 13:13:45 +0530

    Time: 2 ms (37.03%) | Memory: 18.3 MB (18.05%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.cpp
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -11,30 +11,24 @@
  */
 class Solution {
 public:
-    string toStringC(vector<int>arr){
-        string ans="";
-        for(int i=0;i<arr.size();i++){
-            ans+=to_string(arr[i]);
-            if(i==arr.size()-1) continue;
-            ans+="->";
-        }
-        return ans;
-    }
-    void solve(TreeNode* root,vector<int> arr,vector<string>&ans){
-        if(root==NULL) return;
-        arr.push_back(root->val);
-        if(root->left == NULL && root->right == NULL){
-            ans.push_back(toStringC(arr));
+    void solve(TreeNode* root, vector<string>&ans,string s){
+        if(root==NULL){
             return;
         }
-        solve(root->left,arr,ans);
-        solve(root->right,arr,ans);
+        if(!root->left && !root->right){
+            s+=to_string(root->val);
+            ans.push_back(s);
+        }
+
+        s+=to_string(root->val);
+        s+="->";
+        solve(root->left,ans,s);
+        solve(root->right,ans,s);
     }
     vector<string> binaryTreePaths(TreeNode* root) {
         vector<string>ans;
-        if(root==NULL) return ans;
-        vector<int>arr;
-        solve(root,arr,ans);
+        string s;
+        solve(root,ans,s);
         return ans;
     }
 };
