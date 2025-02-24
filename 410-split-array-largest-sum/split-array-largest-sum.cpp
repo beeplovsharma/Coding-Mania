86deaa4a87b8740d968de7f20ac6ad9b03471f01
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-02-24 23:07:46 +0530

    Time: 2 ms (12.09%) | Memory: 10.5 MB (22.90%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool feasible(vector<int>& nums, int k,int threshold){
+        int cnt = 1;
+        int sum = 0;
+
+        for(auto &num:nums){
+            sum+=num;
+
+            if(sum>threshold){
+                sum=num;
+                cnt++;
+
+                if(cnt>k) return false;
+            }
+        }
+
+        return true;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int lo = *max_element(nums.begin(),nums.end());
+        int hi = accumulate(nums.begin(),nums.end(),0);
+
+        while(hi-lo>1){
+            int mid = hi+(lo-hi)/2;
+
+            if(feasible(nums,k,mid)){
+                hi = mid;
+            }
+            else lo=mid+1;
+        }
+
+        if(feasible(nums,k,lo)) return lo;
+        if(feasible(nums,k,hi)) return hi;
+
+        return -1;
+    }
+};
