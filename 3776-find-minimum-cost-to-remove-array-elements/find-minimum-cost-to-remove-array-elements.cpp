1bb851924d274a3046bbafcad3664d6a4e8d5d7e
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-03-02 11:48:03 +0530

    Time: 320 ms (17.65%) | Memory: 181.8 MB (29.41%) - LeetSync

--- 3776-find-minimum-cost-to-remove-array-elements/find-minimum-cost-to-remove-array-elements.cpp
+++ 3776-find-minimum-cost-to-remove-array-elements/find-minimum-cost-to-remove-array-elements.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-
-    int fun(vector<int>& nums,int prev,int ind,vector<vector<int>>&dp){
-        if(ind==nums.size()) return nums[prev];
-        if(ind==nums.size()-1) return max(nums[prev],nums[ind]);
-
-        if(dp[prev][ind]!=-1) return dp[prev][ind];
-
-        int op1 = max(nums[prev],nums[ind]) + fun(nums,ind+1,ind+2,dp);
-        int op2 = max(nums[prev],nums[ind+1]) + fun(nums,ind,ind+2,dp);
-        int op3 = max(nums[ind],nums[ind+1]) + fun(nums,prev,ind+2,dp);
-
-        return dp[prev][ind] = min(op1,min(op2,op3));
-    }
-
     int minCost(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return fun(nums,0,1,dp);
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+
+        for(int prev=0;prev<n;prev++){
+            dp[prev][n-1] = max(nums[prev],nums[n-1]);
+            dp[prev][n] = nums[prev];
+        }
+
+        for(int prev=n-2;prev>=0;prev--){
+            for(int ind=n-2;ind>=1;ind--){
+                int op1 = max(nums[prev],nums[ind]) + dp[ind+1][ind+2];
+                int op2 = max(nums[prev],nums[ind+1]) + dp[ind][ind+2];
+                int op3 = max(nums[ind],nums[ind+1]) + dp[prev][ind+2];
+
+                dp[prev][ind] = min(op1,min(op2,op3));
+            }
+        }
+        return dp[0][1];
     }
 };
