5a02d652ea446525afce1d4ed4043526d72e187d
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-03-11 08:15:51 +0530

    Time: 43 ms (9.68%) | Memory: 16.9 MB (8.98%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,57 +1,26 @@
 class Solution {
 public:
-    int solve(string s,int k){
-        int l = 0 , r = 0;
+    int numberOfSubstrings(string s) {
+        int i=0,j=0;
         int n = s.size();
-        int cnt = 0;
-        unordered_map<char,int>mp;
-        while(r<n){
-            mp[s[r]]++;
 
-            while(l<=r && mp.size()>k){
-                mp[s[l]]--;
-                if(mp[s[l]]==0) mp.erase(s[l]);
-                l++;
+        int cnt = 0;
+
+        unordered_map<char,int>mp;
+        while(j<n){
+            if(s[j]=='b' || s[j]=='a' || s[j]=='c'){
+                mp[s[j]]++;
             }
 
-            if(mp.size()<=k) cnt += r-l+1;
-            r++;
+            while(i<=j && mp.size()==3){
+                cnt+=n-j;
+                mp[s[i]]--;
+                if(mp[s[i]]==0) mp.erase(s[i]);
+                i++;
+            }
+
+            j++;
         }
         return cnt;
-    }
-    int numberOfSubstrings(string s) {
-
-        int x = solve(s,3);
-        int y = solve(s,2);
-        return (x-y);
-
-        // int n = s.size();
-        // int cnt = 0;
-        // vector<int>hash(3,-1);
-        // int r = 0;
-        // while(r<n){
-        //     hash[s[r]-'a'] = r;
-        //     int mini = min(hash[0],min(hash[1],hash[2]));
-        //     cnt = cnt + (1+mini);
-        //     r++;
-        // }
-        // return cnt;
-
-        // Brute
-        // int n = s.size();
-        // int cnt = 0;
-        // for(int i=0;i<n;i++){
-        //     set<char>st;
-        //     for(int j=i;j<n;j++){
-        //         st.insert(s[j]);
-
-        //         // if(st.size()>=3) cnt++;
-        //         if(st.size()>=3){
-        //             cnt += (n-j);
-        //             break;
-        //         }
-        //     }
-        // }
-        // return cnt;
     }
 };
