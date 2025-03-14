aaeefaa184060bdc379699e90f233e6acbc15fe8
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-03-14 21:25:10 +0530

    Time: 4 ms (75.08%) | Memory: 97.9 MB (54.32%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/shifting-letters-ii.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
         int n = s.size();
-        vector<int> temp(n , 0);
-        for (auto &shift : shifts) {
-            int i = shift[0], j = shift[1], direction = shift[2] == 0 ? -1 : 1;
+        vector<int>diffArr(n,0);
+        for(auto &shift:shifts){
+            int l = shift[0];
+            int r = shift[1];
+            int val = shift[2]==0 ? -1 : 1;
 
-            temp[i] += direction;
-            if (j + 1 < n) temp[j + 1] -= direction; 
+            diffArr[l]+=val;
+            if(r+1<n) diffArr[r+1]-=val;
         }
-        int cumshift = 0;
+        for(int i=1;i<n;i++) diffArr[i]+=diffArr[i-1];
+
         for(int i=0;i<n;i++){
-            cumshift += temp[i];
-            int shift = (cumshift%26+26)%26;
-            s[i] = 'a' + (s[i]-'a'+shift)%26;
+            int shiftIdx = (diffArr[i]%26+26)%26;
+            s[i] = 'a' + (s[i]-'a'+shiftIdx)%26;
         }
+
         return s;
     }
 };
