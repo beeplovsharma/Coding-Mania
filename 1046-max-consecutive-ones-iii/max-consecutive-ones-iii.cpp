036e5362e43bbfe8649ff6dae256fb4f5e38e7dd
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-03-15 10:28:13 +0530

    Time: 0 ms (100.00%) | Memory: 59.3 MB (26.13%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -6,16 +6,19 @@ public:
         int zeros = 0;
         int maxi = INT_MIN;
         while(j<n){
+            //operation jo har iteration me check krna hai
             if(nums[j]==0) zeros++;
 
-            while(i<=j && zeros>k){
+            //condition check krne ka
+            while(zeros>k){
                 if(nums[i]==0) zeros--;
                 i++;
             }
-
+            
             maxi = max(maxi,j-i+1);
             j++;
         }
-        return maxi;
+
+        return maxi==INT_MIN ? 0 : maxi;
     }
 };
