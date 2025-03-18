8b73ee240262695f1624634ee4f301184902239a
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-03-18 21:22:23 +0530

    Time: 3 ms (46.67%) | Memory: 61 MB (28.44%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/longest-nice-subarray.cpp
@@ -5,31 +5,15 @@ public:
         int n = nums.size();
         
         int i = 0, j = 0;
-        unordered_map<int, int> mp;
-        int dup = 0;
+        int mask = 0;
 
         while (j < n) {
-            int nn = nums[j];
             
-            for (int k = 31; k >= 0; k--) {
-                if (nn & (1 << k)) { // Checking if bit k is set
-                    if (mp.find(k) != mp.end()) dup++;  // If bit is already present, increment conflict count
-                    mp[k]++;  // Increment count for bit position k
-                }
+            while((mask&nums[j])){
+                mask ^= nums[i];
+                i++;
             }
-
-            while (dup > 0) { // Shrink window while there is conflict
-                int nm = nums[i];
-                for (int k = 31; k >= 0; k--) {
-                    if (nm & (1 << k)) { // If bit is present
-                        mp[k]--; 
-                        if (mp[k] == 0) mp.erase(k);
-                        else dup--;  // Reduce conflict count only if bit is still present
-                    }
-                }
-                i++; // Shrink window
-            }
-
+            mask = (mask|nums[j]);
             maxi = max(maxi, j - i + 1); // Update max subarray length
             j++;
         }
