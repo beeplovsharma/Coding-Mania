756d69f212b04107d5c0050946ac98678514d6bf
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-03-19 22:09:14 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (35.92%) - LeetSync

--- 319-bulb-switcher/bulb-switcher.cpp
+++ 319-bulb-switcher/bulb-switcher.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-        if(n==0 || n==1) return n;
-
-        int cnt = 0;
-        for(long long i=1;i<=n;i++){
-            long long sq = sqrt(i);
-            if(sq*1LL*sq==i) cnt++;
-        }
-        return cnt;
+        return sqrt(n);
     }
 };
