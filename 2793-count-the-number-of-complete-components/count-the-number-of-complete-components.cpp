664dc73473259c8ad72e52efe6c684644d900c60
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-03-22 15:18:44 +0530

    Time: 1062 ms (5.08%) | Memory: 434.1 MB (5.56%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-    void DFS(int node,vector<int>adj[],int &nodeCount,int &edgeCount,vector<bool>&isVisited){
-        isVisited[node]=true;
-        nodeCount += 1;
-        edgeCount += adj[node].size();
-        for(auto nextNode:adj[node]){
-            if(!isVisited[nextNode]){
-                DFS(nextNode,adj,nodeCount,edgeCount,isVisited);
+    void DFS(int node,unordered_map<int,vector<int>>adj,vector<bool>&vis,int &cnode,int &cedge){
+        vis[node]=true;
+        cnode+=1;
+        cedge+=adj[node].size();
+        for(auto &ne:adj[node]){
+            if(!vis[ne]){
+                DFS(ne,adj,vis,cnode,cedge);
             }
         }
     }
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<int>adj[n];
-        for(auto it:edges){
-            int u = it[0];
-            int v = it[1];
+        unordered_map<int,vector<int>>adj;
+        for(auto &edge:edges){
+            int u = edge[0];
+            int v = edge[1];
 
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
 
-        int ans = 0;
-        vector<bool>isVisited(n,0);
+        int comp = 0;
+        vector<bool>vis(n,false);
         for(int i=0;i<n;i++){
-            int nodeCount = 0, edgeCount = 0;
-            if(isVisited[i]) continue;
-            DFS(i,adj,nodeCount,edgeCount,isVisited);
-            if(nodeCount * (nodeCount-1) == edgeCount) ans++;
+            if(!vis[i]){
+                int cnode=0,cedge=0;
+                DFS(i,adj,vis,cnode,cedge);
+                if(cnode*(cnode-1) == cedge) comp++;
+            }
         }
-        return ans;
+
+        return comp;
     }
 };
