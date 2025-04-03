505475f0cf61bacd41385569ae80f413ffbb7736
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-04-03 08:09:36 +0530

    Time: 5 ms (53.20%) | Memory: 91.1 MB (85.24%) - LeetSync

--- 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.cpp
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.cpp
@@ -3,28 +3,14 @@ public:
     typedef long long ll;
     long long maximumTripletValue(vector<int>& nums) {
         int n = nums.size();
-        vector<int>leftMax(n,0);
-        vector<int>rightMax(n,0);
-
-        int maxi = INT_MIN;
+        ll maxi = 0;
+        ll maxDiff = 0;
+        ll ans = 0;
         for(int i=0;i<n;i++){
-            maxi = max(maxi,nums[i]);
-            leftMax[i] = maxi;
+            ans = max(ans,maxDiff * 1LL * nums[i]);
+            maxDiff = max(maxDiff,maxi-nums[i]);
+            maxi = max(maxi,(ll)nums[i]);
         }
-
-        maxi = INT_MIN;
-        for(int i=n-1;i>=0;i--){
-            maxi = max(maxi,nums[i]);
-            rightMax[i] = maxi;
-        }
-
-        ll ans = INT_MIN;
-        for(int i=1;i<n-1;i++){
-            if((leftMax[i-1]-nums[i]) < 0) continue;
-            ll sum = (leftMax[i-1]-nums[i]) * 1LL * rightMax[i+1];
-            ans = max(ans,sum);
-        }
-
-        return (ans==INT_MIN) ? 0 : ans;
+        return ans;
     }
 };
