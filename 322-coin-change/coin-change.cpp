0b97ddae057f275280cdc6dcc1b474095db8efe4
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-04-05 09:36:55 +0530

    Time: 51 ms (24.44%) | Memory: 48.8 MB (36.76%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    int fun(vector<int>& coins, int ind ,int amount,vector<vector<int>>&dp){
-        if(amount==0) return 0;
-        if(ind==coins.size() || amount<0){
-            return 1e9;
-        }
-
-        if(dp[ind][amount]!=-1) return dp[ind][amount];
-
-        int pick = 1 + fun(coins,ind,amount-coins[ind],dp);
-        int notPick = fun(coins,ind+1,amount,dp);
-
-        return dp[ind][amount] = min(pick,notPick);
-    }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        int ans = fun(coins,0,amount,dp);
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+
+        for(int ind=n-1;ind>=0;ind--){
+            for(int tar=1;tar<=amount;tar++){
+                int pick = 1e9,notPick=1e9;
+                if(tar-coins[ind]>=0) pick = 1 + dp[ind][tar-coins[ind]];
+                if(ind+1<n) notPick = dp[ind+1][tar];
+
+                dp[ind][tar] = min(pick,notPick);
+            }
+        }
+
+        int ans = dp[0][amount];
         return ans==1e9 ? -1 : ans;
     }
 };
