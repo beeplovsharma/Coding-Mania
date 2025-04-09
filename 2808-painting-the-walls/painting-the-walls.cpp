ee53bf6216dbfb0519823cb09f1babbcd73c3093
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-04-09 08:50:43 +0530

    Time: 43 ms (80.42%) | Memory: 121.8 MB (34.04%) - LeetSync

--- 2808-painting-the-walls/painting-the-walls.cpp
+++ 2808-painting-the-walls/painting-the-walls.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    int fun(vector<int>& cost, vector<int>& time,int ind,int wallsLeft,vector<vector<int>>&dp){
-        if(wallsLeft<=0) return 0;
-        if(ind==cost.size()) return 1e9; 
-
-        if(dp[ind][wallsLeft]!=-1) return dp[ind][wallsLeft];
-
-        int skip = fun(cost,time,ind+1,wallsLeft,dp);
-        int paint = cost[ind] + fun(cost,time,ind+1,wallsLeft-1-time[ind],dp);
-
-        return dp[ind][wallsLeft] = min(skip,paint);
-    }
     int paintWalls(vector<int>& cost, vector<int>& time) {
         int n = cost.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return fun(cost,time,0,n,dp);
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 1e9));
+
+        for (int i = 0; i <= n; i++) dp[i][0] = 0;
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int wallsLeft = 1; wallsLeft <= n; wallsLeft++) {
+                int skip = dp[ind + 1][wallsLeft];
+
+                int remaining = max(0, wallsLeft - 1 - time[ind]);
+                int paint = cost[ind] + dp[ind + 1][remaining];
+
+                dp[ind][wallsLeft] = min(skip, paint);
+            }
+        }
+
+        return dp[0][n];
     }
+
 };
