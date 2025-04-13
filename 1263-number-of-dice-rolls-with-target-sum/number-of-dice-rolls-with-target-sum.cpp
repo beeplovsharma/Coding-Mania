a9f763e7cef64fd7534ab90aa44a17ed78ae506b
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-04-13 22:16:41 +0530

    Time: 20 ms (27.11%) | Memory: 11.3 MB (36.51%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -2,23 +2,20 @@ class Solution {
 public:
     const int MOD = 1e9 + 7;
 
-    int fun(int dice, int tar, int k,vector<vector<int>>&dp) {
-        if(dice == 0 && tar == 0) return 1;
-        if(dice==0 || tar<0) return 0;
-
-        if(dp[dice][tar]!=-1) return dp[dice][tar];
-        
-        int count = 0;
-        for(int face=1;face<=k;face++){
-            count = (count + fun(dice-1,tar-face,k,dp))%MOD;
-        }
-
-        return dp[dice][tar] = count;
-    }
-
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
-        return fun(n, target, k,dp);
+        vector<vector<int>>dp(n+1,vector<int>(target+1,0));
+        dp[0][0]=1;
+        for(int dice=1;dice<=n;dice++){
+            for(int tar=1;tar<=target;tar++){
+                int count = 0;
+                for(int face=1;face<=k;face++){
+                    if(dice-1>=0 && tar-face>=0) count = (count + dp[dice-1][tar-face])%MOD;
+                }
+
+                dp[dice][tar] = count;
+            }
+        }
+        return dp[n][target];
     }
 
 };
