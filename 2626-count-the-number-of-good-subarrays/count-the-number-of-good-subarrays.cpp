065f857b05cbd06cf36ebc0aa22954cc56453fc2
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-04-16 07:51:12 +0530

    Time: 97 ms (28.20%) | Memory: 79.4 MB (34.36%) - LeetSync

--- 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
@@ -2,23 +2,45 @@ class Solution {
 public:
     #define ll long long
     long long countGood(vector<int>& nums, int k) {
-        int i=0,j=0;
+        int i = 0, j = 0;
         int n = nums.size();
-        ll pair = 0;
+        int p = 0;
         ll ans = 0;
-        map<int,int>mp;
-        int l = 0;
-        for(int j=0;j<n;j++){
-            mp[nums[j]]++;
-            pair += mp[nums[j]]-1;
+        unordered_map<int, pair<int, int>> mp; //[num, {cnt, pairs}]
 
-            while(i<=j && pair>=k){
-                ans+=n-j;
-                mp[nums[i]]--;
-                if(mp[nums[i]]>=1) pair-=mp[nums[i]];
+        while (j < n) {
+            mp[nums[j]].first++;  // increase frequency
+
+            // update pair count for nums[j]
+            int cur_cnt = mp[nums[j]].first;
+            int new_pairs = cur_cnt * (cur_cnt - 1) / 2;
+            p = p - mp[nums[j]].second + new_pairs;
+            mp[nums[j]].second = new_pairs;
+
+            // shrink window while valid
+            while (p >= k) {
+                ans += (n - j);
+
+                // update pair count for nums[i] before removing
+                int cur_cnt_i = mp[nums[i]].first;
+                int old_pairs_i = mp[nums[i]].second;
+
+                mp[nums[i]].first--;  // decrease frequency
+                int new_cnt_i = mp[nums[i]].first;
+                int new_pairs_i = new_cnt_i * (new_cnt_i - 1) / 2;
+
+                p = p - old_pairs_i + new_pairs_i;
+                if (new_cnt_i > 0)
+                    mp[nums[i]].second = new_pairs_i;
+                else
+                    mp.erase(nums[i]);
+
                 i++;
             }
+
+            j++;
         }
+
         return ans;
     }
 };
