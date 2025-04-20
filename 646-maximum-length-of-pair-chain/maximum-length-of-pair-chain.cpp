055803bb6b9e456b8764ed6b714e086c5fe96bd1
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-04-20 23:41:31 +0530

    Time: 170 ms (27.95%) | Memory: 163.4 MB (12.22%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int fun(vector<vector<int>>& pairs, int ind, int prev, vector<vector<int>>& dp) {
-        if (ind == pairs.size()) return 0;
-
-        if (dp[ind][prev + 1] != -1) return dp[ind][prev + 1];
-
-        int skip = fun(pairs, ind + 1, prev, dp);
-        int take = 0;
-        if (prev == -1 || pairs[prev][1] < pairs[ind][0]) {
-            take = 1 + fun(pairs, ind + 1, ind, dp);
-        }
-
-        return dp[ind][prev + 1] = max(skip, take);
-    }
-
     int findLongestChain(vector<vector<int>>& pairs) {
         sort(pairs.begin(), pairs.end());
         int n = pairs.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return fun(pairs, 0, -1, dp);
+        vector<vector<int>> dp(n+1, vector<int>(n + 1, 0));
+        for(int ind=n-1;ind>=0;ind--){
+            for(int prev=ind-1;prev>=-1;prev--){
+                int skip = dp[ind + 1][prev+1];
+                int take = 0;
+                if (prev == -1 || pairs[prev][1] < pairs[ind][0]) {
+                    take = 1 + dp[ind + 1][ind+1];
+                }
+
+                dp[ind][prev + 1] = max(skip, take);
+            }
+        }
+
+        return dp[0][-1+1];
     }
 
 };
