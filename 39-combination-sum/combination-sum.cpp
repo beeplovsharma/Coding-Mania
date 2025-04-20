38ff958cc3bf61d32259e939872cdcdbb70a5b13
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-04-21 00:55:07 +0530

    Time: 29 ms (20.26%) | Memory: 31.7 MB (17.50%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    void fun(vector<int>& nums,int ind,vector<int>&curr ,int target,vector<vector<int>>&ans){
-        if(target==0){
-            ans.push_back(curr);
-            return;
+    vector<vector<int>> fun(int idx, int target, vector<int>& candidates) {
+        if (target == 0) return {{}};
+        if (target < 0 || idx == candidates.size()) return {};
+
+        vector<vector<int>> include = fun(idx, target - candidates[idx], candidates);
+        vector<vector<int>> exclude = fun(idx + 1, target, candidates);
+
+        vector<vector<int>> result;
+
+        for (auto vec : include) {
+            vec.insert(vec.begin(), candidates[idx]);
+            result.push_back(vec);
         }
-        if(target<0 || ind==nums.size()) return;
-        
 
-        fun(nums,ind+1,curr,target,ans); // skip
-        curr.push_back(nums[ind]);
-        fun(nums,ind,curr,target-nums[ind],ans);
-        curr.pop_back();
+        for (auto vec : exclude) {
+            result.push_back(vec);
+        }
 
+        return result;
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int>curr;
-        fun(candidates,0,curr,target,ans);
-        return ans;
+        return fun(0, target, candidates);
     }
 };
