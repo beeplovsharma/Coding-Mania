e132bdeb7df0c22aa8e66a41d4a4eae1229b7e30
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-04-20 23:25:45 +0530

    Time: 271 ms (31.74%) | Memory: 296.3 MB (24.52%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,17 +1,35 @@
-#include<bits/stdc++.h>
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& arr) {
-        vector<int>temp;
-        int n = arr.size();
-        temp.push_back(arr[0]);
-        for(int i=1;i<n;i++){
-            if(arr[i]>temp.back()) temp.push_back(arr[i]);
-            else {
-                int ind = lower_bound(temp.begin(),temp.end(),arr[i])-temp.begin();
-                temp[ind] = arr[i];
+    int fun(vector<int>& nums, int ind, int prevIdx,vector<vector<int>>&dp) {
+        if (ind == nums.size()) {
+            return 0;
+        }
+
+        if (dp[ind][prevIdx+1]!=-1) return dp[ind][prevIdx+1];
+
+        int notPick = fun(nums, ind + 1, prevIdx, dp);
+        int pick = 0;
+        if (prevIdx == -1 || nums[ind] > nums[prevIdx]) {
+            pick = 1 + fun(nums, ind + 1, ind, dp);
+        }
+
+        return dp[ind][prevIdx+1] = max(pick, notPick);
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int ind=n-1;ind>=0;ind--){
+            for(int prevIdx=ind-1;prevIdx>=-1;prevIdx--){
+                int notPick = dp[ind + 1][prevIdx+1];
+                int pick = 0;
+                if (prevIdx == -1 || nums[ind] > nums[prevIdx]) {
+                    pick = 1 + dp[ind + 1][ind+1];
+                }
+                dp[ind][prevIdx+1] = max(pick, notPick);
             }
         }
-        return temp.size();
+        return dp[0][-1+1];
     }
+
 };
