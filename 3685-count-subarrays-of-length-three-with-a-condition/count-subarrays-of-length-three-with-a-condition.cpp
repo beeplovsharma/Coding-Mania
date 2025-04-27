7ba7af55af3e48aff86151e75fecd0670ba3ecfe
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-04-27 20:24:06 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (11.51%) - LeetSync

--- 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.cpp
+++ 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.cpp
@@ -4,9 +4,10 @@ public:
         int n = nums.size();
         int cnt = 0;
         for(int i=0; i+2<n; i++) {
-            if((nums[i] + nums[i+2]) * 2 == nums[i+1]) {
-                cnt++;
-            }
+            float sum = nums[i]+nums[i+2];
+            float half = nums[i+1]/2.0;
+
+            if(sum==half) cnt++;
         }
         return cnt;
     }
