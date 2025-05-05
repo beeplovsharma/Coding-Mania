279342436f25dca5000d0abe83a9a28e6b464dcb
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-05-05 23:31:59 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (96.65%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canFinish(int V, vector<vector<int>>& prerequisites) {
+        vector<int>adj[V];
+        vector<int>indeg(V,0);
+        for(auto &pre:prerequisites){
+            int u = pre[1];
+            int v = pre[0];
+
+            adj[u].push_back(v);
+            indeg[v]++;
+        }
+
+        queue<int>q;
+        for(int i=0;i<V;i++){
+            if(indeg[i]==0) q.push(i);
+        }
+
+        vector<int>ans;
+        while(!q.empty()){
+            int curr = q.front();
+            ans.push_back(curr);
+            q.pop();
+
+            for(auto &v:adj[curr]){
+                indeg[v]--;
+                if(indeg[v]==0) q.push(v);
+            }
+        }
+
+        return ans.size()==V;
+    }
+};
