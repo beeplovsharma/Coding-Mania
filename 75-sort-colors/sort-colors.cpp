20aca331ea742b169bc090b7d9e61403a4d28f98
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-05-17 11:35:31 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (78.60%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,44 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // DNF Algo
-        int n = nums.size();
-        int low = 0,mid=0,high=n-1;
-        while(mid<=high){
-            if(nums[mid]==0){
-                swap(nums[low],nums[mid]);
-                low++;
-                mid++;
-            }
-            else if(nums[mid]==1){
-                mid++;
-            }
-            else if(nums[mid]==2){
-                swap(nums[high],nums[mid]);
-                // mid++;
-                high--;
-            }
+        unordered_map<int,int>mp;
+        for(auto &x:nums){
+            mp[x]++;
         }
 
-        // BRUTE
-        // int cnt0 = 0;
-        // int cnt1 = 0;
-        // int cnt2 = 0;
-        // int n = nums.size();
-        // for(int i=0;i<n;i++){
-        //     if(nums[i]==0) cnt0++;
-        //     else if(nums[i]==1) cnt1++;
-        //     else if(nums[i]==2) cnt2++;
-        // }
+        int zero = mp[0];
+        int one = mp[1];
+        int two = mp[2];
 
-        // for(int i=0;i<cnt0;i++){
-        //     nums[i]=0;
-        // }
-        // for(int i=cnt0;i<cnt0+cnt1;i++){
-        //     nums[i]=1;
-        // }
-        // for(int i=cnt0+cnt1;i<cnt0+cnt1+cnt2;i++){
-        //     nums[i]=2;
-        // }
+        for(int i=0;i<zero;i++) nums[i]=0;
+        for(int i=zero;i<zero+one;i++) nums[i]=1;
+        for(int i=zero+one;i<zero+one+two;i++) nums[i]=2;
+
     }
 };
