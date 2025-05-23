7b19e64c416781287ecb98bbda18705def002895
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-05-24 00:34:51 +0530

    Time: 4 ms (48.39%) | Memory: 126.6 MB (69.03%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
@@ -4,22 +4,21 @@ public:
     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
         ll count = 0;
         ll sum = 0;
-        ll minLoss = LLONG_MAX;  // Use LLONG_MAX for long long
-
-        for (int i = 0; i < nums.size(); i++) {
-            int xorr = nums[i] ^ k;
-            if (xorr > nums[i]) {
+        ll nuksaan = INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            int xorr = nums[i]^k;
+            if(xorr>nums[i]){
                 sum += xorr;
                 count++;
-                minLoss = min(minLoss, (ll)(xorr - nums[i]));
-            } else {
-                sum += nums[i];
-                minLoss = min(minLoss, (ll)(nums[i] - xorr));
+                nuksaan = min(nuksaan,(ll)(abs(xorr-nums[i])));
+            }else{
+                sum+=nums[i];
+                nuksaan = min(nuksaan,(ll)(abs(xorr-nums[i])));
             }
         }
 
-        if (count % 2 == 0) return sum;
+        if(count%2==0) return sum;
 
-        return sum - minLoss;
+        return sum-nuksaan;
     }
 };
