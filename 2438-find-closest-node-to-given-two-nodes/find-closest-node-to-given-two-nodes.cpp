9c84c8e6322dc1b76829bba6562c8585c27bb549
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-05-31 09:42:27 +0530

    Time: 202 ms (12.38%) | Memory: 156.3 MB (13.99%) - LeetSync

--- 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
@@ -1,40 +1,46 @@
 class Solution {
 public:
-    void BFS(vector<int>& edges, int start, int col, vector<vector<int>>& dist) {
-        int n = edges.size();
-        queue<int> q;
+    void BFS(vector<int>& edges,vector<vector<int>>&dist,int start,int col,int n){
+        queue<int>q;
+        vector<bool>vis(n,false);
         q.push(start);
-        dist[start][col] = 0;
+        vis[start]=true;
+        dist[start][col]=0;
 
-        while (!q.empty()) {
+        while(!q.empty()){
             int node = q.front();
             q.pop();
 
-            int next = edges[node];
-            if (next != -1 && dist[next][col] == -1) {
-                dist[next][col] = dist[node][col] + 1;
-                q.push(next);
+            int nextNode = edges[node];
+            if (nextNode != -1 && !vis[nextNode]) {
+                q.push(nextNode);
+                vis[nextNode] = true;
+                dist[nextNode][col] = dist[node][col] + 1;
             }
+
         }
     }
-
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
         int n = edges.size();
-        vector<vector<int>> dist(n, vector<int>(2, -1));
+        vector<vector<int>>dist(n,vector<int>(2,-1));
+        BFS(edges,dist,node1,0,n);
+        BFS(edges,dist,node2,1,n);
 
-        BFS(edges, node1, 0, dist);
-        BFS(edges, node2, 1, dist);
+        int ans = -1;
+        int mini = INT_MAX;
+        for(int i=0;i<n;i++){
+            int a = dist[i][0];
+            int b = dist[i][1];
 
-        int answer = -1, minDist = INT_MAX;
-        for (int i = 0; i < n; ++i) {
-            if (dist[i][0] != -1 && dist[i][1] != -1) {
-                int maxDist = max(dist[i][0], dist[i][1]);
-                if (maxDist < minDist) {
-                    minDist = maxDist;
-                    answer = i;
+            if(a!=-1 && b!=-1){
+                int maxi = max(a,b);
+                if(maxi<mini){
+                    mini = maxi;
+                    ans = i;
                 }
             }
         }
-        return answer;
+
+        return ans;
     }
 };
