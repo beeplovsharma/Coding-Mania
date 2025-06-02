3a7191c28bbd6ed43aee570a33fd90cf3ebada0f
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-06-02 20:53:46 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (11.01%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,46 +1,51 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+    vector<vector<int>>dirs = {{-1,0},{1,0},{0,-1},{0,1}};
 
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        queue<pair<pair<int, int>, int>> q;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    q.push({{i, j}, 0});
-                    vis[i][j] = 2;
+    bool isValid(int i,int j,vector<vector<bool>>&vis,vector<vector<int>>& grid){
+        int n = grid.size(), m = grid[0].size();
+        return (i>=0 && i<n && j>=0 && j<m && !vis[i][j] && grid[i][j]==1);
+    }
+
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        queue<pair<pair<int,int>,int>>q;
+        vector<vector<bool>>vis(n,vector<bool>(m,false));
+
+        int fresh = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){     
+                    q.push({{i,j},0});
+                    vis[i][j] = true;
+                }else if(grid[i][j]==1){
+                    fresh++;
                 }
             }
         }
 
-        int tm = 0;
-        int drow[] = {-1, 0, 1, 0};
-        int dcol[] = {0, 1, 0, -1};
-        while (!q.empty()) {
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t = q.front().second;
-            tm = max(tm, t);
+        int minutes = 0;
+
+
+        while(!q.empty()){
+            int i = q.front().first.first;
+            int j = q.front().first.second;
+            int level = q.front().second;
+            minutes = max(minutes,level);
             q.pop();
 
-            for (int i = 0; i < 4; i++) {
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
-                    grid[nrow][ncol] == 1 && vis[nrow][ncol] == 0) {
-                    q.push({{nrow, ncol}, t+1});
-                    vis[nrow][ncol] = 2;
+            for(auto &dir:dirs){
+                int _i = i+dir[0];
+                int _j = j+dir[1];
+
+                if(isValid(_i,_j,vis,grid)){
+                    q.push({{_i,_j},level+1});
+                    vis[_i][_j]=true;
+                    fresh--;
                 }
             }
         }
-        for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (vis[i][j] == 0 && grid[i][j] == 1)
-                        return -1;
-                }
-            }
-        return tm;
+
+        return fresh>0 ? -1 : minutes;
     }
 };
