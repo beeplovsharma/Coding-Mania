13f009d5c9a60956ec4cbf6a026f640ca9de4cda
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-06-04 18:59:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (75.07%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
-    bool isRightPlaceForQueen(vector<string>&chess,int row,int col,int n){
-        //Check Vertically
-        for(int i=row-1,j=col;i>=0;i--){
-            if(chess[i][j]=='Q') return false;
-        }
-
-        //Check Left-Diagonal
-        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
-            if(chess[i][j]=='Q') return false;
-        }
-
-        //Check Right-Diagonal
-        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++){
-            if(chess[i][j]=='Q') return false;
-        }
-
-        return true;
-    }
-    void fun(int row,vector<string>chess,vector<vector<string>>&ans,int n){
-        if(row==n){
-            ans.push_back(chess);
+    void solve(vector<string>&grid,int row,vector<bool>&cols,vector<bool>&mdiag,vector<bool>&rdiag,vector<vector<string>>&ans){
+        if(row==grid.size()){
+            ans.push_back(grid);
             return;
         }
-
+        
+        int n = cols.size();
         for(int col=0;col<n;col++){
-            if(isRightPlaceForQueen(chess,row,col,n)){
-                chess[row][col] = 'Q';
-                fun(row+1,chess,ans,n);
-                chess[row][col] = '.';
+            if(cols[col]==false && mdiag[row+col]==false && rdiag[row-col+n-1]==false){
+                grid[row][col]='Q';
+                cols[col]=1;
+                mdiag[row+col]=1;
+                rdiag[row-col+n-1]=1;
+                solve(grid,row+1,cols,mdiag,rdiag,ans);
+                grid[row][col]='.';
+                cols[col]=0;
+                mdiag[row+col]=0;
+                rdiag[row-col+n-1]=0;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<string> chess(n, string(n, '.'));
+        vector<bool>cols(n,0);
+        vector<bool>mdiag(2*n-1,0);
+        vector<bool>rdiag(2*n+1,0);
+        vector<string>grid(n, string(n, '.'));
         vector<vector<string>>ans;
-        int row = 0;
-        fun(row,chess,ans,n);
+
+        solve(grid,0,cols,mdiag,rdiag,ans);
+        
         return ans;
     }
 };
