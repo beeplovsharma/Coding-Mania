0c5430639c351d6cc7c0d06c419fff2d0722627b
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-06-04 19:46:05 +0530

    Time: 14 ms (35.21%) | Memory: 24 MB (18.51%) - LeetSync

--- 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.cpp
+++ 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.cpp
@@ -1,55 +1,35 @@
 class Solution {
 public:
-    bool isValid(string &word,unordered_map<char,int>letterHash){
-        for(auto &x:word){
-            letterHash[x]--;
-            if(letterHash[x]<0) return false;
-        }
-        return true;
-    }
+    int solve(vector<string>& words,int ind, unordered_map<char,int>freq,vector<int>& score){
+        if(ind==words.size()) return 0;
 
-    void calcMax(vector<string>&asf,unordered_map<string,int>scoreHash,int &ans){
+        int notTake = solve(words,ind+1,freq,score);
+
         int sc = 0;
-        for(auto &word:asf){
-            sc += scoreHash[word];
+        string word = words[ind];
+        bool flag = true;
+        for(int i=0;i<word.size();i++){
+            char ch = word[i];
+            freq[ch]--;
+            if(freq[ch]<0) flag = false;
+            sc+=score[ch-'a'];
         }
-        ans = max(ans,sc);
-    }
-
-    void solve(vector<string>& words,int ind,unordered_map<char,int>&letterHash,unordered_map<string,int>&scoreHash,int &ans,vector<string>&asf){
-        if(ind==words.size()){
-            calcMax(asf,scoreHash,ans);
-            return;
+        int take = 0;
+        if(flag){
+            take = sc + solve(words,ind+1,freq,score);
+        }
+        for(int i=0;i<word.size();i++){
+            char ch = word[i];
+            freq[ch]++;
         }
 
-        solve(words,ind+1,letterHash,scoreHash,ans,asf); //notPick
+        return max(take,notTake);
 
-        if(!isValid(words[ind],letterHash)) return;
 
-        for(auto &x:words[ind]) letterHash[x]--;
-        asf.push_back(words[ind]);
-        solve(words,ind+1,letterHash,scoreHash,ans,asf); //pick
-        for(auto &x:words[ind]) letterHash[x]++;
-        asf.pop_back();
     }
     int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
-        unordered_map<char,int>letterHash;
-        for(auto &x:letters) letterHash[x]++;
-        unordered_map<string,int>scoreHash;
-        for(auto &word:words){
-            int sc = 0;
-            for(int i=0;i<word.size();i++){
-                char ch = word[i];
-                sc+=score[ch-'a'];
-            }
-            scoreHash[word]=sc;
-        }
-        
-        int ans = 0;
-        vector<string>asf;
-
-        solve(words,0,letterHash,scoreHash,ans,asf);
-
-        return ans;
+        unordered_map<char,int>freq;
+        for(auto &x:letters) freq[x]++;
+        return solve(words,0,freq,score); 
     }
 };
