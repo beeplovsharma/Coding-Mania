1e7897f4fc5b300f8ffde531f4aca6c13d8f4466
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-06-13 12:54:20 +0530

    Time: 1 ms (9.45%) | Memory: 30.4 MB (62.42%) - LeetSync

--- 3747-maximum-difference-between-adjacent-elements-in-a-circular-array/maximum-difference-between-adjacent-elements-in-a-circular-array.cpp
+++ 3747-maximum-difference-between-adjacent-elements-in-a-circular-array/maximum-difference-between-adjacent-elements-in-a-circular-array.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
+    int circularIndex(int i, int n) {
+        return (i % n + n) % n; // handles negative indices correctly
+    }
+
     int maxAdjacentDistance(vector<int>& nums) {
         int n = nums.size();
-        vector<int> doubledArr(2 * n);
-        for (int i = 0; i < 2 * n; ++i) {
-            doubledArr[i] = nums[i % n]; // simulate circular behavior
-        }
-
         int maxi = INT_MIN;
 
-        for(int i=1;i<2*n;i++){
-            maxi = max(maxi,abs(doubledArr[i]-doubledArr[i-1]));
+        for (int i = 1; i < 2 * n; i++) {
+            int idx = circularIndex(i, n);
+            int prevIdx = circularIndex(i - 1, n);
+
+            maxi = max(maxi, abs(nums[idx] - nums[prevIdx]));
         }
 
         return maxi;
