bee0f844fe85877c28d38b1eabcc8b88e825c8bf
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-07-03 06:43:26 +0530

    Time: 3 ms (97.66%) | Memory: 12 MB (58.75%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    bool checkPalindrome(string &s,int i,int j,vector<vector<int>>&dp){
-        if(i>=j) return 1;
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        if(s[i]==s[j]) return dp[i][j] = checkPalindrome(s,i+1,j-1,dp);
-        
-        return dp[i][j] = 0;
-    }
     string longestPalindrome(string s) {
         int n = s.size();
-        int maxLen = INT_MIN;
-        int sp=0;
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(checkPalindrome(s,i,j,dp)){
-                    int len = (j-i+1);
-                    if(len > maxLen){
-                        maxLen = len;
-                        sp = i;
-                    }
-                }
+        string ans = "";
+
+        //odd check
+        for(int ax=0;ax<n;ax++){
+            int orbl = ax-1;
+            int orbr = ax+1;
+            while(orbl>=0 && orbr<n && s[orbl]==s[orbr]){
+                orbl--;
+                orbr++;
             }
+            int len = orbr-orbl-1;
+            if(len>ans.size()) ans = s.substr(orbl+1,len);
         }
 
-        return s.substr(sp,maxLen);
+        //even check
+        for(int ax=0;ax<n-1;ax++){
+            int orbl = ax;
+            int orbr = ax+1;
+            while(orbl>=0 && orbr<n && s[orbl]==s[orbr]){
+                orbl--;
+                orbr++;
+            }
+            int len = orbr-orbl-1;
+            if(len>ans.size()) ans = s.substr(orbl+1,len);
+        }
+
+        return ans;
     }
 };
