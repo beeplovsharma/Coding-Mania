05732fd1bc4752aca2108301c36ba61019c63352
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-07-03 05:30:48 +0530

    Time: 11 ms (51.55%) | Memory: 11.4 MB (86.50%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
+    bool checkDup(vector<int>&hash){
+        bool flag = false;
+        for(int i=0;i<256;i++){
+            if(hash[i]>1) return true;
+        }
+        return false;
+    }
     int lengthOfLongestSubstring(string s) {
-        int i=0,j=0;
+        vector<int>hash(256,0);
         int n = s.size();
-        map<char,int>mp;
-        int maxi = INT_MIN;
+        int i=0,j=0;
+        int maxi = 0;
         while(j<n){
-            mp[s[j]]++;
+            hash[s[j]]++;
 
-            while(i<=j && mp.size()!=j-i+1){
-                mp[s[i]]--;
-                if(mp[s[i]]==0) mp.erase(s[i]);
+            while(i<n && checkDup(hash)){
+                hash[s[i]]--;
                 i++;
             }
 
             maxi = max(maxi,j-i+1);
             j++;
         }
-        return maxi==INT_MIN ? 0:maxi;
+        return maxi;
     }
 };
