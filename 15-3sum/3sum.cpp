ca703ba463df31447d62e93294d527f27f264de2
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-07-04 06:40:47 +0530

    Time: 1282 ms (7.23%) | Memory: 291.6 MB (11.76%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,18 +4,16 @@ public:
        sort(nums.begin(),nums.end());
         int n = nums.size();
        set<vector<int>>st;
-       int tar = 0;
        for(int i=0;i<n;i++){
-        int ntar = tar-nums[i];
         int l = i+1;
         int r = n-1;
 
         while(l<r){
-            if(nums[l]+nums[r]==ntar){
+            if(nums[i]+nums[l]+nums[r]==0){
                 st.insert({nums[i],nums[l],nums[r]});
                 l++;
                 r--;
-            }else if(nums[l]+nums[r]<ntar){
+            }else if(nums[i]+nums[l]+nums[r]<0){
                 l++;
             }else{
                 r--;
