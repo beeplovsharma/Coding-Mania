7582230c58931f7872cf0000e79bcd549161e597
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-08-07 09:30:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (32.79%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int i = 0, j = 0;
-        while(i<s.size() && j<t.size()){
+        int i=0,j=0;
+
+        int n = s.size();
+        int m = t.size();
+
+        while(i<n && j<m){
             if(s[i]==t[j]){
                 i++;
                 j++;
-            }
-            else{
+            }else{
                 j++;
             }
         }
-        if(i==s.size()) return true;
 
-        return false;
+        return i==n ? true : false;
     }
 };
