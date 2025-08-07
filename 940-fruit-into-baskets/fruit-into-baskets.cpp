47ccf9f1565f25b2ffaca3c83dec757aab0ce950
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-08-07 22:10:43 +0530

    Time: 42 ms (45.97%) | Memory: 75.2 MB (28.81%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int totalFruit(vector<int>& nums) {
-        int i=0,j=0;
         int n = nums.size();
-        unordered_map<int,int>mp;
+        int i=0;
         int maxi = 0;
-        while(j<n){
+        unordered_map<int,int>mp;
+        for(int j=0;j<n;j++){
             mp[nums[j]]++;
-            
-            while(mp.size()>2){
+            while(i<n && mp.size()>2){
                 mp[nums[i]]--;
                 if(mp[nums[i]]==0) mp.erase(nums[i]);
                 i++;
             }
 
             maxi = max(maxi,j-i+1);
-            j++;
         }
 
         return maxi;
