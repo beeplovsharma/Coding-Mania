513e715e6647e3aa9dfdafc498cc7c28d409f9db
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-08-07 09:25:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (94.27%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
+        int n = s.size();
+        int l=0,r=n-1;
 
-        if(s=="0P") return false;
-
-        string ans = "";
-        for(int i=0;i<s.size();i++){
-
-            if((s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z') || (s[i]>='0' && s[i]<='9')){
-                if(s[i]>='0' && s[i]<='9'){
-                    ans += s[i];
-                }
-
-                else{
-                    ans += (s[i] | ' ');
-                }
+        while(l<=r){
+            if(!isalnum(s[l])){
+                l++;
+                continue;
+            }
+            if(!isalnum(s[r])){
+                r--;
+                continue;
             }
 
-            // s[i] = (s[i] | ' ');
-            // if(s[i]>='a' && s[i]<='z') ans = ans + s[i];
+            if(tolower(s[l])!=tolower(s[r])){
+                return false;
+            }
+
+            l++;
+            r--;
         }
 
-        int left = 0, right = ans.size()-1;
-        bool flag = true;
-        while(left<right){
-            if(ans[left]!=ans[right]){
-                flag = false;
-                break;
-            }
-            else{
-                left++;
-                right--;
-            }
-        }
-        return flag;
+        return true;
     }
 };
