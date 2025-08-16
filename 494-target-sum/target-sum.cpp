a0b438432cb8b6e51b55eadd43c62ec86a484862
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-08-17 04:46:17 +0530

    Time: 319 ms (29.30%) | Memory: 79.1 MB (6.11%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int fun(vector<int>& nums, int ind ,int tar,unordered_map<string,int>&dp){
-        if(ind==nums.size()){
-            return tar==0;
+    int fun(vector<int>& nums,int ind, int target,unordered_map<string,int>&dp){
+        if(ind==0){
+            if(target==0 && nums[ind]==0) return 2;
+            if(nums[ind]==target || nums[ind]==-target) return 1;
+            else return 0;
         }
 
-        string key = to_string(ind)+"_"+to_string(tar);
-        if(dp.count(key)) return dp[key];
-
-        int add = fun(nums,ind+1,tar+nums[ind],dp);
-        int sub = fun(nums,ind+1,tar-nums[ind],dp);
+        string key = to_string(ind)+"_"+to_string(target);
+        if(dp.find(key)!=dp.end()) return dp[key];
+        
+        int add = fun(nums,ind-1,target+nums[ind],dp);
+        int sub = fun(nums,ind-1,target-nums[ind],dp);
 
         return dp[key] = (add+sub);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         unordered_map<string,int>dp;
-        return fun(nums,0,target,dp);
+        return fun(nums,n-1,target,dp);
     }
 };
