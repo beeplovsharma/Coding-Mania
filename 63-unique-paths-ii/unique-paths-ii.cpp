9051cb6c4ec947620ebd1e22015786f445754c34
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-08-16 13:01:35 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (94.20%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,47 +1,24 @@
 class Solution {
 public:
-
-    int fun(int i,int j,vector<vector<int>>& mat,vector<vector<int>>& dp){
-        if(i>=0 && j>=0 && mat[i][j]==1) return 0;
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0) return 0;
+    int dp[101][101];
+    int fun(vector<vector<int>>& grid,int i,int j,int n,int m){
+        if(i>=n || j>=m) return 0;
+        if(i==n-1 && j==m-1) return 1;
 
         if(dp[i][j]!=-1) return dp[i][j];
 
-        int up = fun(i-1,j,mat,dp);
-        int left = fun(i,j-1,mat,dp);
+        int down = 0;
+        if(i+1<n && grid[i+1][j]!=1) down = fun(grid,i+1,j,n,m);
+        int right = 0;
+        if(j+1<m && grid[i][j+1]!=1) right = fun(grid,i,j+1,n,m);
 
-        return dp[i][j] = up+left;
+        return dp[i][j] = down+right;
     }
-
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int n = obstacleGrid.size();
-        int m = obstacleGrid[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        // return fun(n-1,m-1,obstacleGrid,dp);
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i>=0 && j>=0 && obstacleGrid[i][j]==1) dp[i][j] = 0;
-
-                    
-                else if(i==0 && j==0){
-                    dp[i][j] = 1;
-                }
-
-                else{
-                    int up = 0;
-                    if(i-1>=0) 
-                        up = dp[i-1][j];
-
-                    int left = 0;
-                    if(j-1>=0)
-                        left = dp[i][j-1];
-
-                    dp[i][j] = up+left;
-                }
-            }
-        }
-        return dp[n-1][m-1];
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        if(grid[0][0]) return 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        memset(dp,-1,sizeof(dp));
+        return fun(grid,0,0,n,m);
     }
 };
