61624f73d3c6839f26d04b12ce7dc7f74f553ba7
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-08-17 04:53:12 +0530

    Time: 15 ms (75.45%) | Memory: 16.2 MB (74.84%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     int dp[301][5001];
     int fun(vector<int>& coins,int ind,int amt){
-        if(ind==coins.size()){
-            return amt==0;
+        if(ind==0){
+            if(amt%coins[ind]==0) return 1;
+            else return 0;
         }
 
         if(dp[ind][amt]!=-1) return dp[ind][amt];
 
-        int skip = fun(coins,ind+1,amt);
+        int skip = fun(coins,ind-1,amt);
         int choose = 0;
         if(amt-coins[ind]>=0) choose = fun(coins,ind,amt-coins[ind]);
 
-        return dp[ind][amt] = (skip + choose);
+        return dp[ind][amt] = (skip+choose);
     }
     int change(int amount, vector<int>& coins) {
         memset(dp,-1,sizeof(dp));
-        return fun(coins,0,amount);
+        int n = coins.size();
+        return fun(coins,n-1,amount);
     }
 };
