729efd2a8fe3186d3bfb08a7f88ddefe9304f656
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-08-17 04:14:31 +0530

    Time: 29 ms (64.09%) | Memory: 13.3 MB (98.00%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int dp[13][10001];
-    int fun(vector<int>& coins,int ind, int tamt){
-        if(ind==coins.size()){
-            if(tamt==0) return 0;
-            return 1e9;
+    int fun(vector<int>& nums,int ind, int tar){
+        if(ind==0){
+            if(tar%nums[ind]==0) return tar/nums[ind];
+            else return 1e9;
         }
-        if(dp[ind][tamt]!=-1) return dp[ind][tamt];
-        //skip
-        int skip = fun(coins,ind+1,tamt);
-        //choose
-        int choose = 1e9;
-        if(tamt-coins[ind]>=0) 
-            choose = 1 + fun(coins,ind,tamt-coins[ind]);
 
-        return dp[ind][tamt] = min(skip,choose);
+        if(dp[ind][tar]!=-1) return dp[ind][tar];
+
+        int skip = fun(nums,ind-1,tar);
+        int choose = 1e9;
+        if(tar-nums[ind]>=0) choose = 1+fun(nums,ind,tar-nums[ind]);
+
+        return dp[ind][tar] = min(skip,choose);
     }
     int coinChange(vector<int>& coins, int amount) {
         memset(dp,-1,sizeof(dp));
-        int ans =fun(coins,0,amount);
-        return ans >= 1e9 ? -1 : ans;
+        int n = coins.size();
+        int ans = fun(coins,n-1,amount);
+        return ans>=1e9 ? -1 : ans;
     }
 };
