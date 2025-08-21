5ba934fe7ee399eb4044be046fc7d0d4191e90da
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-08-21 21:36:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (47.80%) - LeetSync

---
+++ 886-score-of-parentheses/score-of-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        stack<int>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(') st.push(-1);
+            else{
+                if(st.top()==-1){
+                    st.pop();
+                    st.push(1);
+                }else{
+                    int val = 0;
+                    while(st.top()!=-1){
+                        val += st.top();
+                        st.pop();
+                    }
+                    st.pop();
+                    st.push(2*val);
+                }
+            }
+        }
+
+        int val = 0;
+        while(!st.empty()){
+            val += st.top();
+            st.pop();
+        }
+        return val;
+    }
+};
