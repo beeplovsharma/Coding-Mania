a55715c5a7da3fbf0e60228e110e5dd4ee935fb8
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-09-01 23:27:23 +0530

    Time: 7 ms (59.70%) | Memory: 14.4 MB (20.11%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int dp[501][501];
-    int fun(string &s1, string &s2, int i, int j){
-        if(j<0) return i+1;
-        if(i<0) return j+1;
+    int fun(string &s1, string &s2, int i, int j,vector<vector<int>>&dp){
+        if(j==0) return i;
+        if(i==0) return j;
 
         if(dp[i][j]!=-1) return dp[i][j];
 
         int mini = INT_MAX;
-        if(s1[i]==s2[j]){
-            mini = min(mini,fun(s1,s2,i-1,j-1));
+        if(s1[i-1]==s2[j-1]){
+            mini = min(mini,fun(s1,s2,i-1,j-1,dp));
         }else{
-            int ins = 1 + fun(s1,s2,i,j-1);
-            int del = 1 + fun(s1,s2,i-1,j);
-            int repl = 1 + fun(s1,s2,i-1,j-1);
+            int ins = 1 + fun(s1,s2,i,j-1,dp);
+            int del = 1 + fun(s1,s2,i-1,j,dp);
+            int repl = 1 + fun(s1,s2,i-1,j-1,dp);
 
             mini = min({mini,ins,del,repl});
         }
@@ -22,7 +21,7 @@ public:
     }
     int minDistance(string s1, string s2) {
         int n = s1.size(), m = s2.size();
-        memset(dp,-1,sizeof(dp));
-        return fun(s1,s2,n-1,m-1);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return fun(s1,s2,n,m,dp);
     }
 };
