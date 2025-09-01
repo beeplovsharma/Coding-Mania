09df18844bcff503f695b0be37c73093d8138b5d
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-09-01 23:48:41 +0530

    Time: 5 ms (71.98%) | Memory: 10.3 MB (94.66%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -21,26 +21,27 @@ public:
     }
     int minDistance(string s1, string s2) {
         int n = s1.size(), m = s2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<=n;i++) dp[i][0] = i;
-        for(int j=0;j<=m;j++) dp[0][j] = j;
+        vector<int>cur(m+1,0), prev(m+1,0);
+        for(int j=0;j<=m;j++) prev[j] = j;
 
         for(int i=1;i<=n;i++){
+            cur[0]=i;
             for(int j=1;j<=m;j++){
                 int mini = INT_MAX;
                 if(s1[i-1]==s2[j-1]){
-                    mini = min(mini,dp[i-1][j-1]);
+                    mini = min(mini,prev[j-1]);
                 }else{
-                    int ins = 1 + dp[i][j-1];
-                    int del = 1 + dp[i-1][j];
-                    int repl = 1 + dp[i-1][j-1];
+                    int ins = 1 + cur[j-1];
+                    int del = 1 + prev[j];
+                    int repl = 1 + prev[j-1];
 
                     mini = min({mini,ins,del,repl});
                 }
 
-                dp[i][j] = mini;
+                cur[j] = mini;
             }
+            prev = cur;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
