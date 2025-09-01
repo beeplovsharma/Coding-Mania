5db13983f7263e990587a7c38a1bd83c64b1d9be
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-09-01 22:14:46 +0530

    Time: 32 ms (43.27%) | Memory: 44.3 MB (31.14%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    typedef long long ll;
     int fun(string &s, string &t,int i,int j,vector<vector<int>>&dp){
         if(j==0) return 1;
         if(i==0) return 0;
@@ -18,7 +19,22 @@ public:
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return fun(s,t,n,m,dp);
+        vector<vector<ll>>dp(n+1,vector<ll>(m+1,0));
+        //base case
+        for(int i=0;i<=n;i++) dp[i][0]=1;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                int ans = 0;
+                if(s[i-1]==t[j-1]){
+                    ans = dp[i-1][j-1] + dp[i-1][j];
+                }else{
+                    ans = dp[i-1][j];
+                }
+                dp[i][j] = ans;
+            }
+        }
+
+        return dp[n][m];
     }
 };
