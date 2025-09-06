7685ad5fa55663340d58df6f8613aedd2fdab9bc
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-09-06 22:02:21 +0530

    Time: 1167 ms (5.00%) | Memory: 26.8 MB (50.84%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int dp[2001][2001];
+    bool fun(string &s, string &p,int i,int j){
+        if(i<0 && j<0) return true;
+        if(i<0 && j>=0){
+            for(int k=j;k>=0;k--){
+                if(p[k]!='*') return false;
+            }
+            return true;
+        }
+        if(i>=0 && j<0){
+            return false;
+        }
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(s[i]==p[j] || p[j]=='?'){
+            if(fun(s,p,i-1,j-1)) return dp[i][j] = true;
+        }else if(p[j]=='*'){
+            if(fun(s,p,i,j-1) || fun(s,p,i-1,j)) return dp[i][j] = true;
+        }
+
+        return dp[i][j] = false;
+    }
+    bool isMatch(string s, string p) {
+        memset(dp,-1,sizeof(dp));
+        int n = s.size();
+        int m = p.size();
+        return fun(s,p,n-1,m-1);
+    }
+};
