8107adc28d2de5eef13023602dd43ddc729b37e4
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2025-12-08 22:45:16 +0530

    Time: 277 ms (9.08%) | Memory: 25.8 MB (5.02%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -11,32 +11,21 @@
  */
 class Solution {
 public:
-    struct DiaPair{
-        int height;
-        int diameter;
-    };
+    int height(TreeNode* root){
+        if(root==NULL) return 0;
 
-    DiaPair fun(TreeNode* root){
-        if(root==NULL){
-            DiaPair base;
-            base.height = -1; // height of null is -1
-            base.diameter = 0;
-            return base;
-        }
+        int lh = height(root->left);
+        int rh = height(root->right);
 
-        DiaPair left = fun(root->left);   // faith
-        DiaPair right = fun(root->right); // faith
-
-        DiaPair self;
-        self.height = max(left.height, right.height) + 1;
-
-        int pathThroughRoot = left.height + right.height + 2;
-        self.diameter = max({left.diameter, right.diameter, pathThroughRoot}); // expectation
-
-        return self;
+        return 1+max(lh,rh);
     }
-
     int diameterOfBinaryTree(TreeNode* root) {
-        return fun(root).diameter;
+        if(root==NULL) return 0;
+        
+        int ld = diameterOfBinaryTree(root->left);
+        int rd = diameterOfBinaryTree(root->right);
+        int lrh = height(root->left)+height(root->right);
+
+        return max({ld,rd,lrh});
     }
 };
