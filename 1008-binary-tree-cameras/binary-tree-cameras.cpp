08e704be255cbce85364eb754eb7a33f9a9640f4
Beeplov Sharma <65328299+beeplovsharma@users.noreply.github.com>
2026-01-25 22:37:10 +0530

    Time: 0 ms (100.00%) | Memory: 21.2 MB (88.58%) - LeetSync

--- 1008-binary-tree-cameras/binary-tree-cameras.cpp
+++ 1008-binary-tree-cameras/binary-tree-cameras.cpp
@@ -1,32 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int cameras=0;
-    // 0:has camera, 1:covered, 2:needs camera
+    int cameras;
+
+    // 0:has camera | 1:needs camera | 2:covered
     int fun(TreeNode* root){
-        if(root==NULL){
-            return 1;
-        }
+        if(root==NULL) return 2;
 
         int lh = fun(root->left);
         int rh = fun(root->right);
 
-        if(lh==2 || rh==2){
+        if(lh==1 || rh==1){
             cameras++;
-            return {0};
+            return 0;
         }
 
         if(lh==0 || rh==0){
-            return {1};
+            return 2;
         }
 
-        return {2};
+        return 1;
     }
-
     int minCameraCover(TreeNode* root) {
-        int rh = fun(root);
-
-        if(rh==2) cameras++;
-
-        return cameras;
+        cameras=0;
+        int res = fun(root);
+        if(res==1) cameras++;
+        return cameras;   
     }
 };
